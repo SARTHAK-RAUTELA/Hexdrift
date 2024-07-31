4d6cda9e9b71f4932fcb2c839fa3ae859cac3ef5
sarthakrautela <sarthakrautela80@gmail.com>
2024-07-31 11:22:24 +0530

    css js

--- local_testing/Local1/variation/js.js
+++ local_testing/Local1/variation/js.js
@@ -1,127 +1,96 @@
-(function () {
-  try {
-    /* main variables */
-    var debug = 0;
-    var variation_name = "Cre_sfg20";
-    /* all Pure helper functions */
-    function waitForElement(selector, trigger, delayInterval, delayTimeout) {
-      var interval = setInterval(function () {
-        if (document && document.querySelector(selector) && document.querySelectorAll(selector).length > 0) {
-          clearInterval(interval);
-          trigger();
+if (!window.location.href.includes("https://www.gendergp.com/next-steps/")){
+
+(() => {
+  var __defProp = Object.defineProperty;
+  var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
+
+  // lib/bm-js/lib/index.js
+  var waitFor = /* @__PURE__ */ __name((check, invoke, poll) => {
+    if (check()) {
+      invoke();
+      return;
+    }
+    let polling = setInterval(() => {
+      try {
+        if (check()) {
+          invoke();
+          clearInterval(polling);
+          polling = null;
         }
-      }, delayInterval);
-      setTimeout(function () {
-        clearInterval(interval);
-      }, delayTimeout);
-    }
-    function addLiveEventListener(selector, event, callback, context) {
-      // Helper for enabling IE 8 event bindings
-      function addEvent(el, type, handler) {
-        if (el.attachEvent) el.attachEvent('on' + type, handler);
-        else el.addEventListener(type, handler);
+      } catch (e) {
+        console.info("listener not processed");
       }
-      // Matches polyfill
-      this.Element && function (ElementPrototype) {
-        ElementPrototype.matches = ElementPrototype.matches ||
-          ElementPrototype.matchesSelector ||
-          ElementPrototype.webkitMatchesSelector ||
-          ElementPrototype.msMatchesSelector ||
-          function (selector) {
-            var node = this,
-              nodes = (node.parentNode || node.document).querySelectorAll(selector),
-              i = -1;
-            while (nodes[++i] && nodes[i] != node);
-            return !!nodes[i];
-          };
-      }(Element.prototype);
-      // Live binding helper using matchesSelector
-      function live(selector, event, callback, context) {
-        addEvent(context || document, event, function (e) {
-          var found, el = e.target || e.srcElement;
-          while (el && el.matches && el !== context && !(found = el.matches(selector))) el = el.parentElement;
-          if (found) callback.call(el, e);
-        });
+    }, poll.interval);
+    setTimeout(() => {
+      if (!polling)
+        return;
+      clearInterval(polling);
+      console.info("render listener timeout", poll);
+      window.evolvRenderTimeout = {
+        msg: "evolv render listener timeout",
+        poll
+      };
+    }, poll.duration);
+  }, "waitFor");
+
+  // src/conversion-rate-experts/GenderGP-Test7/variation2/index.js
+  var CRET3hero = `
+  <div class="CRE-T7-hero-container">
+	  <div class="CRE-T7-hero-text">
+		  <h1 class="CRE-T7-hero-heading">
+			  Skip the NHS's <span class="pink-underline">5-10 year </span> wait times. Get gender-altering medications in just 3-5 days. 
+		  </h1>
+		  <div class="CRE-T7-hero-bullets-section">
+			  <div class="CRE-T7-bullet-point">
+				  <p class="CRE-T7-bullet-text">
+					 
+ <span class="bullet-bold">We organise medical transition private prescriptions so you can quickly and easily access the care you need,</span>  with compassionate guidance every step of the way.
+
+
+				  </p>
+			  </div>
+			  <div class="CRE-T7-bullet-point">
+				  <p class="CRE-T7-bullet-text">
+                    <span class="bullet-bold">Join thousands of transgender and non-binary people we\u2019ve helped transition</span>  and support their health and wellbeing. You are not alone! 
+					
+				  </p>
+			  </div>
+			 
+			 
+		  </div>
+		  <div class="cre-ctastart">
+			<div class="journey"> 
+			  <a href="https://www.gendergp.com/accessing-gender-affirming-care/" class="Crestart-journey-button"> Get my medication</a>
+			</div>
+			
+		  </div>
+	  </div>
+	  <div class="CRE-T7-hero-image">
+		  <img src="https://cdn-3.convertexperiments.com/uf/10007679/10007617/banner-min.jpg" width="850" />
+            <div class="cre-text-image">
+            
+"Without GenderGP, I would have had to wait 5-7 years for HRT through the NHS. I would be dead if it were not for GGP. Now I\u2019m 11 months on HRT, my life has done a 180\xB0.\u201D    
+             
+            </div>                                                
+                                                            
+            <div>  \u2014Customer survey respondent, Jul 2024</div>
+	  </div>
+  </div>
+  </div>
+  `;
+  waitFor(
+    () => document.querySelector('[class*="css_mix_blend_mode_passthrough"] + div .et_pb_text_inner'),
+    () => {
+      document.querySelector("body").classList.add("cre-7");
+      var sliderDiv = document.querySelector("#welcome-gate");
+      if (!document.querySelector(".CRE-T7-hero-container")) {
+        sliderDiv && sliderDiv.insertAdjacentHTML("afterend", CRET3hero);
       }
-      live(selector, event, callback, context);
+    },
+    {
+      interval: 50,
+      duration: 15e3
     }
-    try {
-      window.addEventListener("message", function (event) {
-        if (event.data.type == "PLAYER_PLAY") {
-          window._conv_q = window._conv_q || [];
-          _conv_q.push(["triggerConversion", "100033272"]);
-        }
-      });
-    } catch (error) {
-      console.log("error excuting the script ===", error);
-    }
-
-
-
-    var banner = `
-    <div class="Cre_Mainbanner">
-         <div class="content-wrapper">
-    <div class="Cre_bannerleftsection">
-      <h2>The <span>industry standard</span> for building maintenance</h2>
-      <p class="Cre_paragraph">Only SFG20 offers comprehensive maintenance schedules, accessed via cutting-edge software, to help users control risk, achieve compliance, and ensure buildings are safe and efficient.</p>
-      <p class="logoheading">Developed & referenced by:</p>
-      <ul>
-      <li><img src="https://d27c6j8064skg9.cloudfront.net/ConversionRateExpert/SFG20/SFG05/BESA Logo 1.svg"></li>
-      <li><img src="https://d27c6j8064skg9.cloudfront.net/ConversionRateExpert/SFG20/SFG05/RICS Logo 1.svg"></li>
-      <li><img src="https://d27c6j8064skg9.cloudfront.net/ConversionRateExpert/SFG20/SFG05/BSRIA White 1.svg"></li>
-      <li><img src="https://d27c6j8064skg9.cloudfront.net/ConversionRateExpert/SFG20/SFG05/CIBSE Logo 1.svg"></li>
-        <li><img src="https://d27c6j8064skg9.cloudfront.net/ConversionRateExpert/SFG20/SFG05/image+22.svg"></li>
-        
-      </ul>
-     </div>
-
-     <div class="hs-video-widget" data-hsv-embed-id="01b0757b-f1db-4529-83c3-81f6fb48da57" data-hsv-status="loaded">
-     <div class="hs-video-container" style="max-width: 1920px; margin: 0 auto;">
-       <div class="hs-video-wrapper" style="position: relative; height: 0; padding-bottom: 56.25%">
-       <iframe referrerpolicy="origin" sandbox="allow-forms allow-scripts allow-same-origin allow-popups" allow="autoplay; fullscreen;" style="position: absolute !important; width: 100% !important; height: 100% !important; left: 0; top: 0; border: 0 none; pointer-events: initial" title="HubSpot Video" loading="lazy" data-hsv-id="89515008748" data-hsv-style="" data-hsv-width="1920" data-hsv-height="1080" data-hsv-autoplay="false" data-hsv-loop="false" data-hsv-muted="false" data-hsv-hidden-controls="false" data-hsv-full-width="false" src="https://play-eu1.hubspotvideo.com/v/139678265/id/89515008748?parentOrigin=https%3A%2F%2Fwww.sfg20.co.uk&amp;renderContext=hubl-iframe#hsvid=01b0757b-f1db-4529-83c3-81f6fb48da57"></iframe>
-       </div>
-     </div>
-     <p class="Crevideocontent"> Our Facilities-iQ software uses color-coding to distinguish between legally required tasks and those that are optimal and discretionary.</P>
-     </div>
-     
-     </div>
-     </div>
-   <div class="Cre_bottomimg">	   <img src="https://cdn-3.convertexperiments.com/uf/10007679/10007638/Bottom.png"></di>
-    `;
-
-
-    function init() {
-      // Insert the new span element after the .th_homepage-heroheading element
-      if (!document.querySelector('.Cre_Mainbanner')) {
-        document.querySelector('.new-homepage-banner-v2 > div').insertAdjacentHTML('afterend', banner);
-
-      }
-
-    }
-    /* Initialise variation */
-    function sfg20(list, observer) {
-      list.getEntries().forEach((entry) => {
-        if (entry.entryType === "mark" && entry.name === "afterHydrate") {
-          observer.disconnect();
-          clearInterval(sfg20interval);
-          waitForElement("body", init, 50, 15000);
-          window.isHydrated = true;
-        }
-      });
-    }
-    if (!window.isHydrated) {
-      var sfg20interval = setInterval(function () {
-        waitForElement("body", init, 50, 15000);
-      }, 50);
-      setTimeout(function () {
-        clearInterval(sfg20interval);
-      }, 3000);
-      const observer = new PerformanceObserver(sfg20);
-      observer.observe({ entryTypes: ["mark"] });
-    } else {
-      waitForElement("body", init, 50, 15000);
-    }
-  } catch (e) {
-    if (debug) console.log(e, "error in Test" + variation_name);
-  }
+  );
 })();
+}
