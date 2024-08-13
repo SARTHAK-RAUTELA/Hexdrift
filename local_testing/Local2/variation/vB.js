5a00f067054683c8963057b891a8a43caf4400e1
sarthakrautela <sarthakrautela80@gmail.com>
2024-08-13 13:03:02 +0530

    js css

--- local_testing/Local2/variation/vB.js
+++ local_testing/Local2/variation/vB.js
@@ -1,121 +1,121 @@
 (function () {
-  try {
-      /* main variables */
-      var debug = 1;
-      var variation_name = "cre-sfg-14";
-      /* helper library */
-      var _$;
-      !(function (factory) {
-          _$ = factory();
-      })(function () {
-          var bm = function (s) {
-              if (typeof s === "string") {
-                  this.value = Array.prototype.slice.call(document.querySelectorAll(s));
-              }
-              if (typeof s === "object") {
-                  this.value = [s];
-              }
-          };
-          bm.prototype = {
-              eq: function (n) {
-                  this.value = [this.value[n]];
-                  return this;
-              },
-              each: function (fn) {
-                  [].forEach.call(this.value, fn);
-                  return this;
-              },
-              log: function () {
-                  var items = [];
-                  for (var index = 0; index < arguments.length; index++) {
-                      items.push(arguments[index]);
-                  }
-                  console && console.log(variation_name, items);
-              },
-              addClass: function (v) {
-                  var a = v.split(" ");
-                  return this.each(function (i) {
-                      for (var x = 0; x < a.length; x++) {
-                          if (i.classList) {
-                              i.classList.add(a[x]);
-                          } else {
-                              i.className += " " + a[x];
-                          }
-                      }
-                  });
-              },
-              live: function (selector, event, callback, context) {
-                  /****Helper Functions****/
-                  // helper for enabling IE 8 event bindings
-                  function addEvent(el, type, handler) {
-                      if (el.attachEvent) el.attachEvent("on" + type, handler);
-                      else el.addEventListener(type, handler);
-                  }
-                  // matches polyfill
-                  this.Element &&
-                      (function (ElementPrototype) {
-                          ElementPrototype.matches =
-                              ElementPrototype.matches ||
-                              ElementPrototype.matchesSelector ||
-                              ElementPrototype.webkitMatchesSelector ||
-                              ElementPrototype.msMatchesSelector ||
-                              function (selector) {
-                                  var node = this,
-                                      nodes = (node.parentNode || node.document).querySelectorAll(selector),
-                                      i = -1;
-                                  while (nodes[++i] && nodes[i] != node);
-                                  return !!nodes[i];
-                              };
-                      })(Element.prototype);
-                  // live binding helper using matchesSelector
-                  function live(selector, event, callback, context) {
-                      addEvent(context || document, event, function (e) {
-                          var found,
-                              el = e.target || e.oldElement;
-                          while (el && el.matches && el !== context && !(found = el.matches(selector))) el = el.parentElement;
-                          if (found) callback.call(el, e);
-                      });
-                  }
-                  live(selector, event, callback, context);
-              },
-              waitForElement: function (selector, trigger, delayInterval, delayTimeout) {
-                  var interval = setInterval(function () {
-                      if (_$(selector).value.length) {
-                          clearInterval(interval);
-                          trigger();
-                      }
-                  }, delayInterval);
-                  setTimeout(function () {
-                      clearInterval(interval);
-                  }, delayTimeout);
-              },
-          };
-          return function (selector) {
-              return new bm(selector);
-          };
-      });
-      function waitForSwiper(trigger) {
-          var interval = setInterval(function () {
-              if (typeof window.Swiper != "undefined") {
-                  clearInterval(interval);
-                  trigger();
-              }
-          }, 50);
-          setTimeout(function () {
-              clearInterval(interval);
-          }, 15000);
-      }
-      // Helper functions
-      function addScript() {
-          var bmScript = document.createElement("script");
-          bmScript.src = "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js";
-          document.getElementsByTagName("head")[0].appendChild(bmScript);
-          var bmcsslink = document.createElement("link");
-          bmcsslink.rel = "stylesheet";
-          bmcsslink.href = "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css";
-          document.getElementsByTagName("head")[0].appendChild(bmcsslink);
-      }
-      var CreSlider = `
+    try {
+        /* main variables */
+        var debug = 1;
+        var variation_name = "cre-sfg-14";
+        /* helper library */
+        var _$;
+        !(function (factory) {
+            _$ = factory();
+        })(function () {
+            var bm = function (s) {
+                if (typeof s === "string") {
+                    this.value = Array.prototype.slice.call(document.querySelectorAll(s));
+                }
+                if (typeof s === "object") {
+                    this.value = [s];
+                }
+            };
+            bm.prototype = {
+                eq: function (n) {
+                    this.value = [this.value[n]];
+                    return this;
+                },
+                each: function (fn) {
+                    [].forEach.call(this.value, fn);
+                    return this;
+                },
+                log: function () {
+                    var items = [];
+                    for (var index = 0; index < arguments.length; index++) {
+                        items.push(arguments[index]);
+                    }
+                    console && console.log(variation_name, items);
+                },
+                addClass: function (v) {
+                    var a = v.split(" ");
+                    return this.each(function (i) {
+                        for (var x = 0; x < a.length; x++) {
+                            if (i.classList) {
+                                i.classList.add(a[x]);
+                            } else {
+                                i.className += " " + a[x];
+                            }
+                        }
+                    });
+                },
+                live: function (selector, event, callback, context) {
+                    /****Helper Functions****/
+                    // helper for enabling IE 8 event bindings
+                    function addEvent(el, type, handler) {
+                        if (el.attachEvent) el.attachEvent("on" + type, handler);
+                        else el.addEventListener(type, handler);
+                    }
+                    // matches polyfill
+                    this.Element &&
+                        (function (ElementPrototype) {
+                            ElementPrototype.matches =
+                                ElementPrototype.matches ||
+                                ElementPrototype.matchesSelector ||
+                                ElementPrototype.webkitMatchesSelector ||
+                                ElementPrototype.msMatchesSelector ||
+                                function (selector) {
+                                    var node = this,
+                                        nodes = (node.parentNode || node.document).querySelectorAll(selector),
+                                        i = -1;
+                                    while (nodes[++i] && nodes[i] != node);
+                                    return !!nodes[i];
+                                };
+                        })(Element.prototype);
+                    // live binding helper using matchesSelector
+                    function live(selector, event, callback, context) {
+                        addEvent(context || document, event, function (e) {
+                            var found,
+                                el = e.target || e.oldElement;
+                            while (el && el.matches && el !== context && !(found = el.matches(selector))) el = el.parentElement;
+                            if (found) callback.call(el, e);
+                        });
+                    }
+                    live(selector, event, callback, context);
+                },
+                waitForElement: function (selector, trigger, delayInterval, delayTimeout) {
+                    var interval = setInterval(function () {
+                        if (_$(selector).value.length) {
+                            clearInterval(interval);
+                            trigger();
+                        }
+                    }, delayInterval);
+                    setTimeout(function () {
+                        clearInterval(interval);
+                    }, delayTimeout);
+                },
+            };
+            return function (selector) {
+                return new bm(selector);
+            };
+        });
+        function waitForSwiper(trigger) {
+            var interval = setInterval(function () {
+                if (typeof window.Swiper != "undefined") {
+                    clearInterval(interval);
+                    trigger();
+                }
+            }, 50);
+            setTimeout(function () {
+                clearInterval(interval);
+            }, 15000);
+        }
+        // Helper functions
+        function addScript() {
+            var bmScript = document.createElement("script");
+            bmScript.src = "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js";
+            document.getElementsByTagName("head")[0].appendChild(bmScript);
+            var bmcsslink = document.createElement("link");
+            bmcsslink.rel = "stylesheet";
+            bmcsslink.href = "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css";
+            document.getElementsByTagName("head")[0].appendChild(bmcsslink);
+        }
+        var CreSlider = `
 <div class="cre-t-39-testimonial-wrapper" style="display:none">
 <div class="cre-t-39-testimonial-container ">
 <div class="cre_headinger">
@@ -133,11 +133,11 @@
 <p>Tailor and organize maintenance schedules with custom Maintenance Regimes, then securely share them with your team in the field.</p>
 </div>
 <div class="cre-t-sfg14-box swiper-slide">
-<img src="https://d27c6j8064skg9.cloudfront.net/ConversionRateExpert/SFG20/SFG14/dashboard_screens_3.svg">
+<img src="https://cdn-3.convertexperiments.com/uf/10007679/10007638/dashboard_screens_3-(1).jpg">
 <p>Customize task details like duration, frequency, criticality and skill set for better resource planning and allocation.</p>
 </div>
 <div class="cre-t-sfg14-box swiper-slide">
-<img src="https://d27c6j8064skg9.cloudfront.net/ConversionRateExpert/SFG20/SFG14/dashboard_screens_4.svg">
+<img src="https://cdn-3.convertexperiments.com/uf/10007679/10007638/dashboard_screens_4.jpg">
 <p>Create tailored maintenance schedules with Derived or Custom options, perfect for focusing on critical tasks and managing specialist assets.</p>
 </div>
 <div class="cre-t-sfg14-box swiper-slide">
@@ -180,45 +180,42 @@
 <!-- </div> -->
 </div>
 `;
-      function intializeSwiper() {
-          var swiper = document.querySelector(".cre-t-sfg14-boxslider");
-          new Swiper(swiper, {
-              slidesPerView: 1.5,
-              centeredSlides: true,
-              grabCursor: true,
-              spaceBetween: 30,
-              loop: true,
-              // autoplay: {
-              //     delay: 2000, // 2000 milliseconds = 2 seconds
-              //     disableOnInteraction: false,
-              // },
-              pagination: {
-                  el: ".cre-swiper-pagination",
-                  clickable: true,
-              },
-              navigation: {
-                  nextEl: ".cre-swiper-button-next",
-                  prevEl: ".cre-swiper-button-prev",
-              },
-          });
-      }
-      var helper = _$();
-      function init() {
-          _$("body").addClass(variation_name);
-          if (!document.querySelector(".cre-t-39-testimonial-container")) {
-              document.querySelector(".body-container--home .widget-type-cell .row-number-1").insertAdjacentHTML("afterend", CreSlider);
-          }
-          waitForSwiper(
-              function () {
-                  intializeSwiper();
-              },
-              50,
-              15000
-          );
-      }
-      addScript();
-      helper.waitForElement(".body-container--home .widget-type-cell .row-number-1", init, 50, 25000);
-  } catch (e) {
-      if (debug) console.log(e, "error in Test" + variation_name);
-  }
+        function intializeSwiper() {
+            var swiper = document.querySelector(".cre-t-sfg14-boxslider");
+            new Swiper(swiper, {
+                slidesPerView: 1.5,
+                centeredSlides: true,
+                grabCursor: true,
+                spaceBetween: 30,
+                loop: true,
+               
+                pagination: {
+                    el: ".cre-swiper-pagination",
+                    clickable: true,
+                },
+                navigation: {
+                    nextEl: ".cre-swiper-button-next",
+                    prevEl: ".cre-swiper-button-prev",
+                },
+            });
+        }
+        var helper = _$();
+        function init() {
+            _$("body").addClass(variation_name);
+            if (!document.querySelector(".cre-t-39-testimonial-container")) {
+                document.querySelector(".body-container--home .widget-type-cell .row-number-1").insertAdjacentHTML("afterend", CreSlider);
+            }
+            waitForSwiper(
+                function () {
+                    intializeSwiper();
+                },
+                50,
+                15000
+            );
+        }
+        addScript();
+        helper.waitForElement(".body-container--home .widget-type-cell .row-number-1", init, 50, 25000);
+    } catch (e) {
+        if (debug) console.log(e, "error in Test" + variation_name);
+    }
 })();
