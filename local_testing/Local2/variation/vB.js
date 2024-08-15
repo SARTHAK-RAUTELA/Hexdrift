7be0cf0130cd3d81676dc39c101081c6df73e532
sarthakrautela <sarthakrautela80@gmail.com>
2024-08-15 09:28:05 +0530

    js

--- local_testing/Local2/variation/vB.js
+++ local_testing/Local2/variation/vB.js
@@ -1,215 +1,195 @@
 (function () {
-    try {
-        /* main variables */
-        var debug = 0;
-        var variation_name = "TT-148";
-        /* all Pure helper functions */
-        function waitForElement(selector, trigger, delayInterval, delayTimeout) {
-            var interval = setInterval(function () {
-                if (document && document.querySelector(selector) && document.querySelectorAll(selector).length > 0) {
-                    clearInterval(interval);
-                    trigger();
-                }
-            }, delayInterval);
-            setTimeout(function () {
-                clearInterval(interval);
-            }, delayTimeout);
-        }
-        function addLiveEventListener(selector, event, callback, context) {
-            // Helper for enabling IE 8 event bindings
-            function addEvent(el, type, handler) {
-              if (el.attachEvent) el.attachEvent('on' + type, handler);
-              else el.addEventListener(type, handler);
-            }
-            // Matches polyfill
-            this.Element && function (ElementPrototype) {
-              ElementPrototype.matches = ElementPrototype.matches ||
-                ElementPrototype.matchesSelector ||
-                ElementPrototype.webkitMatchesSelector ||
-                ElementPrototype.msMatchesSelector ||
-                function (selector) {
-                  var node = this,
-                    nodes = (node.parentNode || node.document).querySelectorAll(selector),
-                    i = -1;
-                  while (nodes[++i] && nodes[i] != node);
-                  return !!nodes[i];
-                };
-            }(Element.prototype);
-            // Live binding helper using matchesSelector
-            function live(selector, event, callback, context) {
-              addEvent(context || document, event, function (e) {
-                var found, el = e.target || e.srcElement;
-                while (el && el.matches && el !== context && !(found = el.matches(selector))) el = el.parentElement;
-                if (found) callback.call(el, e);
-              });
-            }
-            live(selector, event, callback, context);
-          }
-        var thumbtechservices = `
-            <div class="thumbtechservices">
-                <h2 class="bmheading">Popular services near you.</h2>
-                <div class="bm-item">
-                    <li title="House cleaning"><a href="https://www.thumbtack.com/instant-results/?zip_code=90066&keyword_pk=102906936611670860&project_pk=517046450169815067"><img src="https://d27c6j8064skg9.cloudfront.net/Thumbtack/TT+-+144+%7C+HP+Hero+Redesign/Sparkle_Icon_%C2%B7_Medium.png"> <p>House <br>Cleaning</p></a></li>
-                    <li title="Handyman"><a href="https://www.thumbtack.com/instant-results/?zip_code=90066&keyword_pk=102906936628587357&project_pk=516161021990002698"><img src="https://d27c6j8064skg9.cloudfront.net/Thumbtack/TT+-+144+%7C+HP+Hero+Redesign/DIY-Effort_Icon_%C2%B7_Medium.png" > <p>Handyman <br> <span>j</span></p></a></li>
-                    <li title="Local electricians" ><a href="https://www.thumbtack.com/instant-results/?zip_code=90066&keyword_pk=367799061344665605&project_pk=517046561932238849"><img src="https://d27c6j8064skg9.cloudfront.net/Thumbtack/TT+-+144+%7C+HP+Hero+Redesign/Energy_Icon_%C2%B7_Medium.png"> <p>Electrical and <br> Wiring Repair</p></a></li>
-                    <li><a href="https://www.thumbtack.com/instant-results/?zip_code=90066&keyword_pk=108249668856752917&project_pk=517046758073319441"><img src="https://d27c6j8064skg9.cloudfront.net/Thumbtack/TT+-+144+%7C+HP+Hero+Redesign/Brush_Icon_%C2%B7_Medium-1.png"> <p>Interior<br> Painting</p></a></li>
-                    <li><a href="https://www.thumbtack.com/instant-results/?zip_code=90066&keyword_pk=228629991346899932&project_pk=517046785496023060"><img src="https://d27c6j8064skg9.cloudfront.net/Thumbtack/TT+-+144+%7C+HP+Hero+Redesign/Trash_Icon_%C2%B7_Medium.png"> <p>Junk <br> Removal</p></a></li>
-                    <li><a href="https://www.thumbtack.com/instant-results/?zip_code=90066&keyword_pk=367799060310671361&project_pk=517046805709824017"><img src="https://d27c6j8064skg9.cloudfront.net/Thumbtack/TT+-+144+%7C+HP+Hero+Redesign/Dolly_Icon_%C2%B7_Medium.png"> <p>Local Moving<br>(Under 50 miles)</p></a></li>
-                    <li class="bmhideservice"><a href="https://www.thumbtack.com/instant-results/?zip_code=90066&keyword_pk=367799053227180037&project_pk=517046842712686592"><img src="https://d27c6j8064skg9.cloudfront.net/Thumbtack/TT+-+144+%7C+HP+Hero+Redesign/Repair-Support_Icon_%C2%B7_Medium.png"> <p>Appliance Repair or Maintenance</p></a></li>
-                    <li class="bmhideservice"><a href="https://www.thumbtack.com/instant-results/?zip_code=90066&keyword_pk=367799059811311616&project_pk=517046868953260048"><img src="https://d27c6j8064skg9.cloudfront.net/Thumbtack/TT+-+144+%7C+HP+Hero+Redesign/Preview-Carousel_Icon_%C2%B7_Medium.png"> <p>Floor Installation or Replacement</p></a></li>
-                </div>
-            </div>
-        `;
-        var reviews = `
-            <div class="reviewsection desktop">
-                <p> Trusted by +4.5M people <span> • </span>4.9/5 <span><img src="https://d27c6j8064skg9.cloudfront.net/Thumbtack/TT+-+144+%7C+HP+Hero+Redesign/Star-Filled_Icon_%C2%B7_Small.png"></span> with over 300k reviews on the App Store</p>
-            </div>
-        `;
-        var mobilereviews = `
-            <div class="reviewsection mobile">
-                <p> Trusted by +4.5M people <span>4.9/5 <img src="https://d27c6j8064skg9.cloudfront.net/Thumbtack/TT+-+144+%7C+HP+Hero+Redesign/Star-Filled_Icon_%C2%B7_Small.png">with over 300k reviews on the App Store</span></p>
-            </div>
-        `;
-        var imagesforappsection = ` <img src="https://production-next-images-cdn.thumbtack.com/i/511992410826965002/width/400.png">`;
-       
-       
-        var heroheading = 
-        `<div class="thmobilebanner"><img src="//cdn.optimizely.com/img/20611073899/2b43f5d6b9954b86a2a9df7fd4b9f96e.png"></div>
-        <h1 class="mb5  homepage-hero_heavy">
-            <div class="homepage-hero_textCarousel">
-                <ul class="homepage-hero_scroll">
-                    <li>Home improvement,</li>
-                    <li>Home repair,</li>
-                    <li>Home inspection,</li>
-                    <li>Home cleaning,</li>
-                    <li>Home improvement,</li>
-                </ul>
-            </div>
-            <br>made easy.
-        </h1> `;
-        /* Variation Init */
-        function init() {
-            document.querySelector("body").classList.add(variation_name);
+	try {
+		/* main variables */
+		var debug = 1;
+		var variation_name = "cre-t-5";
 
-            waitForElement('form[class*="search-bar-form_root"] input[data-test="search-input"]',function(){
-                document.querySelector('form[class*="search-bar-form_root"] input[data-test="search-input"]').setAttribute("placeholder","Describe your project or problem")
-            },50,15000)
+		function waitForElement(
+			selector,
+			trigger,
+			delayInterval,
+			delayTimeout
+		) {
+			var interval = setInterval(function () {
+				if (document.querySelector(selector)) {
+					clearInterval(interval);
+					trigger();
+				}
+			}, delayInterval);
+			setTimeout(function () {
+				clearInterval(interval);
+			}, delayTimeout);
+		}
+		function live(selector, event, callback, context) {
+			/* Helper Functions */
+			// Helper for enabling IE 8 event bindings
+			function addEvent(el, type, handler) {
+				if (el.attachEvent) el.attachEvent("on" + type, handler);
+				else el.addEventListener(type, handler);
+			}
+			// Matches polyfill
+			this.Element &&
+				(function (ElementPrototype) {
+					ElementPrototype.matches =
+						ElementPrototype.matches ||
+						ElementPrototype.matchesSelector ||
+						ElementPrototype.webkitMatchesSelector ||
+						ElementPrototype.msMatchesSelector ||
+						function (selector) {
+							var node = this,
+								nodes = (node.parentNode || node.document).querySelectorAll(
+									selector
+								),
+								i = -1;
+							while (nodes[++i] && nodes[i] != node);
+							return !!nodes[i];
+						};
+				})(Element.prototype);
+			// Live binding helper using matchesSelector
+			function live(selector, event, callback, context) {
+				addEvent(context || document, event, function (e) {
+					var found,
+						el = e.target || e.srcElement;
+					while (
+						el &&
+						el.matches &&
+						el !== context &&
+						!(found = el.matches(selector))
+					)
+						el = el.parentElement;
+					if (found) callback.call(el, e);
+				});
+			}
+			live(selector, event, callback, context);
+		}
+		function setSessionCookie(cookieName, cookieValue) {
+			document.cookie = `${cookieName}=${cookieValue}; path=/`;
+		}
 
-            waitForElement('[class*="homepage-hero_mainSection"] [class*="homepage-hero_mobileSearchBar"] [class*="faux-search-input_root"]',function(){
-                document.querySelector('[class*="homepage-hero_mainSection"] [class*="homepage-hero_mobileSearchBar"] [class*="faux-search-input_root"] span.truncate').innerHTML = "Describe your project or problem"
-            },50,15000)
+		var html = `
+	  <div class="paste_custom">
+		<span>Paste your text here</span>
+		<span class="blinking_cursor">|</span>
+	  </div>`;
 
-            var textValuesToCheck = ['Popular services in '];
-            var elementsToModify = document.querySelectorAll('[class*="Type_title"]');
-            elementsToModify.forEach((element) => {
-                var fullTextContent = element.textContent.trim();
-                if (textValuesToCheck.some(value => fullTextContent.startsWith(value))) {
-                    var parentElement = element.closest('.bg-white');
-                    if (parentElement) {
-                        parentElement.classList.add('bmhidesection');
-                    }
-                }
-            });
-            if (document.querySelector('.bmhidesection [class*="Type_title5"]')) {
-                if (!document.querySelector(".thumbtechservices")) {
-                    document.querySelector('.bmhidesection [class*="Type_title5"]').insertAdjacentHTML('afterend', thumbtechservices);
-                }
-            } else {
-                var rootSiblingElement = document.querySelector('[data-testid="root"] + div');
-                if (rootSiblingElement) {
-                    if (!document.querySelector(".thumbtechservices")) {
-                        rootSiblingElement.insertAdjacentHTML('afterend', thumbtechservices);
-                    }
-                } else {
-                    var customerHeaderSiblingElement = document.querySelector('[class*="composable-customer-header"] + div');
-                    if (customerHeaderSiblingElement) {
-                        if (!document.querySelector(".thumbtechservices")) {
-                            customerHeaderSiblingElement.insertAdjacentHTML('afterend', thumbtechservices);
-                        }
-                    }
-                }
-            }
-            // Putting new heading
-            if (!document.querySelector(".thmobilebanner")) {
-                document.querySelector('html body [class*="homepage-hero_heavy"]').insertAdjacentHTML('afterend', heroheading);
-            
-            }
-                
-            if (!document.querySelector(".reviewsection.desktop")) {
-                document.querySelector('#uniqueId4 ~ [class*="search-bar_zipCodeError"]').insertAdjacentHTML('afterend', reviews);
-            }
-            if (!document.querySelector('.reviewsection.mobile')) {
-                document.querySelector('#uniqueId4 ~ [class*="search-bar_zipCodeError"]').insertAdjacentHTML('afterend', mobilereviews);
-            }
-            function wrapDivs() {
-                // Select the starting div with class bmhidesection
-                var startDiv = document.querySelector('.bmhidesection');
-                if (!startDiv) return; // Exit if starting div is not found
-                // Select the next sibling divs after startDiv until we wrap 4 divs
-                var siblingDivs = [];
-                var currentDiv = startDiv.nextElementSibling;
-                for (var i = 0; i < 2; i++) {
-                    if (currentDiv && currentDiv.tagName === 'DIV' && !currentDiv.classList.contains('wrapped-divs')) {
-                        siblingDivs.push(currentDiv);
-                        currentDiv = currentDiv.nextElementSibling;
-                    } else {
-                        break; // Exit loop if not enough valid sibling divs found
-                    }
-                }
-                // Create a new parent div
-                var newParentDiv = document.createElement('div');
-                newParentDiv.className = 'wrapped-divs'; // Assign a class to the new parent div for styling purposes
-                // Append the selected divs to the new parent div
-                siblingDivs.forEach(function (div) {
-                    newParentDiv.appendChild(div);
-                });
-                // Insert the new parent div after startDiv
-                startDiv.parentNode.insertBefore(newParentDiv, startDiv.nextSibling);
-                // Check if the .wrapped-divs element is empty, then remove it
-                var wrappedDiv = document.querySelector('.wrapped-divs');
-                if (wrappedDiv && !wrappedDiv.hasChildNodes()) {
-                    wrappedDiv.parentNode.removeChild(wrappedDiv);
-                }
-            }
-            waitForElement(".bmhidesection", function () {
-                wrapDivs()
-            }, 50, 15000);
-            // Get the zip code value from the input field
-      var zipCode = document.querySelector('[class*="search-bar_zipCodeInput"]').value;
-      // Select all the links inside the thumbtechservices div
-      var links = document.querySelectorAll('.thumbtechservices a');
-      // Loop through each link and replace the zip code in the href attribute
-      links.forEach(function(link) {
-          var href = link.getAttribute('href');
-          href = href.replace(/zip_code=([0-9]+)/, 'zip_code=' + zipCode);
-          link.setAttribute('href', href);
-      });
-    
-        }
-        /* Initialise variation */
-        function thumbtackTest144(list, observer) {
-            list.getEntries().forEach((entry) => {
-                if (entry.entryType === "mark" && entry.name === "afterHydrate") {
-                    observer.disconnect();
-                    clearInterval(test144Interval);
-                    waitForElement("body", init, 50, 15000);
-                    window.isHydrated = true;
-                }
-            });
-        }
-        if (!window.isHydrated) {
-            var test144Interval = setInterval(function () {
-                waitForElement("body", init, 50, 15000);
-            }, 50);
-            setTimeout(function () {
-                clearInterval(test144Interval);
-            }, 3000);
-            const observer = new PerformanceObserver(thumbtackTest144);
-            observer.observe({ entryTypes: ["mark"] });
-        } else {
-            waitForElement("body", init, 50, 15000);
-        }
-    } catch (e) {
-        if (debug) console.log(e, "error in Test" + variation_name);
-    }
-  })();
+		function addTextAndCursor() {
+			document.querySelector("textarea").placeholder = "";
+			document.querySelector("textarea").nextElementSibling.classList.add("cre-t-5_hide");
+			document.querySelector("textarea").insertAdjacentHTML("afterend", html);
+
+		}
+
+
+
+
+
+		function eventListener() {
+
+			live('#paste', 'input', function () {
+				waitForElement('.paste_custom', function () {
+					setTimeout(function () {
+						if (!document.querySelector('.paste_custom').classList.contains('cre-t-5_hide')) {
+							document.querySelector('.paste_custom').classList.add('cre-t-5_hide');
+						}
+					}, 100);
+				}, 50, 15000);
+			})
+
+			live("#bCheckForAi", "mousedown", function (event) {
+				var textarea = document.querySelector('textarea');
+				if (!textarea.contains(event.target)) {
+					if (textarea.value.length >= 50) {
+						var checkStarted = false;
+						var intervalId = setInterval(function () {
+
+							if (checkStarted === false && document.querySelector("#bCheckForAi").textContent.trim().toLowerCase().includes("checking")) {
+								document.querySelector("#bCheckForAi").classList.add("cre-t-5_checking");
+								checkStarted = true;
+							}
+							else if (checkStarted === true && document.querySelector("#bCheckForAi").textContent.trim().toLowerCase().includes("ai")) {
+								document.querySelector("#bCheckForAi").classList.remove("cre-t-5_checking");
+
+								clearInterval(intervalId);
+							}
+						}, 250);
+					}
+				}
+			});
+
+			live("#gLightmode", "click", function () {
+				document.querySelector(".paste_custom").classList.toggle("cre-t-5_dark");
+			});
+			live("textarea", "click", function () {
+				document.querySelector(".paste_custom").classList.add("cre-t-5_hide");
+			});
+			document.addEventListener('click', function (event) {
+				var textarea = document.querySelector('textarea');
+
+				// Check if the clicked element is not the textarea and not a descendant of the textarea
+				if (!textarea.contains(event.target)) {
+					// Perform the desired action\
+					if (textarea.value.length < 1) {
+						document.querySelector(".paste_custom").classList.remove("cre-t-5_hide");
+					}
+				}
+			});
+		}
+
+		function modifyButtonSection() {
+			document.querySelector("#bCheckForAi").parentElement.classList.add("cre-t-5_button-container");
+			document.querySelector("#bCheckForAi").parentElement.previousElementSibling.classList.add("cre-t-5_hide_desktop");
+
+			//  modify button text
+			// document.querySelector("#bCheckForAi").textContent = "IS THIS DETECTED AS AI?"
+			// document.querySelector("#bHumanize").textContent = "HUMANIZE THIS TEXT"
+
+		}
+
+		function modifySubCopy() {
+			document.querySelector("#cre-lightbox-bottom-sec >div > .Text").classList.add("cre-t-5_subcopy");
+			setTimeout(() => {
+				document.querySelector("#cre-lightbox-bottom-sec >div > .Text").textContent = 'Instantly assess the likelihood of AI detection across all major tools:';
+			}, 500);
+
+		}
+
+		function updateBoxFooter() {
+			document.querySelector("#cre-lightbox-middle-sec #gLightmode").parentElement.classList.add("cre-t-5_box-footer");
+		}
+
+		function init() {
+
+			setTimeout(function () {
+				var isLoggedIn = localStorage.getItem("logged_in");
+				if (isLoggedIn === null || isLoggedIn === "False") {
+					setSessionCookie("logged_out_users", "true");
+					if (!document.querySelector("." + variation_name)) {
+						document.body.classList.add(variation_name);
+						addTextAndCursor();
+						modifyButtonSection();
+						modifySubCopy();
+
+						waitForElement(".paste_custom", function () {
+							if (!window.cre_5_event) {
+								eventListener();
+								window.cre_5_event = true;
+							}
+						}, 50, 5000);
+
+						waitForElement("#cre-lightbox-middle-sec #gLightmode", function () {
+							updateBoxFooter()
+						}, 50, 15000);
+
+					}
+				} else {
+					setSessionCookie("logged_out_users", "false");
+				}
+			}, 500)
+
+		}
+		waitForElement('html[lang="en"] textarea', init, 50, 25000);
+
+	} catch (e) {
+		if (debug) console.log(e, "error in Test" + variation_name);
+	}
+})();
