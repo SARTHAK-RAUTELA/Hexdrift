2af37129fa5ca015a4903bdc5e99d70b6604459b
sarthakrautela <sarthakrautela80@gmail.com>
2024-08-30 18:31:07 +0530

    js

--- local_testing/Local1/variation/js.js
+++ local_testing/Local1/variation/js.js
@@ -1,105 +1,64 @@
 (function () {
-    try {
+  try {
       /* main variables */
       var debug = 1;
-      var variation_name = "cre-t-56";
-  
-      /* helper library */
-      var _$;
-      !(function (factory) {
-        _$ = factory();
-      })(function () {
-        var bm = function (s) {
-          if (typeof s === "string") {
-            this.value = Array.prototype.slice.call(document.querySelectorAll(s));
-          }
-          if (typeof s === "object") {
-            this.value = [s];
-          }
-        };
-        bm.prototype = {
-          eq: function (n) {
-            this.value = [this.value[n]];
-            return this;
-          },
-          each: function (fn) {
-            [].forEach.call(this.value, fn);
-            return this;
-          },
-          log: function () {
-            var items =[];
-            for (let index = 0; index < arguments.length; index++) {
-              items.push(arguments[index]);
-            }
-            console && console.log(variation_name, items);
-          },
-          addClass: function (v) {
-            var a = v.split(" ");
-            return this.each(function (i) {
-              for (var x = 0; x < a.length; x++) {
-                if (i.classList) {
-                  i.classList.add(a[x]);
-                } else {
-                  i.className += " " + a[x];
-                }
+      var variation_name = "SEA211-SWO";
+
+      function waitForElement(selector, trigger) {
+          var interval = setInterval(function () {
+              if (
+                  document &&
+                  document.querySelector(selector) &&
+                  document.querySelectorAll(selector).length > 0
+              ) {
+                  clearInterval(interval);
+                  trigger();
               }
-            });
-          },
-          waitForElement: function (
-            selector,
-            trigger,
-            delayInterval,
-            delayTimeout
-          ) {
-            var interval = setInterval(function () {
-              if (_$(selector).value.length) {
-                clearInterval(interval);
-                trigger();
-              }
-            }, delayInterval);
-            setTimeout(function () {
+          }, 50);
+          setTimeout(function () {
               clearInterval(interval);
-            }, delayTimeout);
-          },
-        };
-        return function (selector) {
-          return new bm(selector);
-        };
-      });
-  
-      var helper = _$();
-  
-  
-      function addHeroImg(){
-          helper.waitForElement('.container .homepage-page-header img[alt="Great Offer - "]', function(){
-            var imageElement = document.querySelector('.container .homepage-page-header img[alt="Great Offer - "]');
-            if (imageElement) {
-                imageElement.src = 'https://cdn-3.convertexperiments.com/uf/10007679/10007537/desk-v1.gif';
-            }
-          }, 50, 25000)
+          }, 15000);
       }
-  
-      function addHeroImgMob(){
-        helper.waitForElement('.container .homepage-page-header img[alt="Latest Offer - "]', function(){
-          var imageElement = document.querySelector('.container .homepage-page-header img[alt="Latest Offer - "]');
-          if (imageElement) {
-              imageElement.src = 'https://cdn-3.convertexperiments.com/uf/10007679/10007537/mob-v1.gif';
-          }
-        }, 50, 25000)
+
+      // function for pushing the Optimixely events----------
+      function pushOptimizelyEvents(event) {
+          window['optimizely'] = window['optimizely'] || [];
+          window['optimizely'].push({
+              type: "event",
+              eventName: event,
+              tags: {
+                  revenue: 0, // Optional in cents as integer (500 == $5.00)
+                  value: 0.00 // Optional as float
+              }
+          });
       }
-  
+
       /* Variation Init */
       function init() {
-        _$('body').addClass(variation_name)
-  
-        addHeroImg()
-        addHeroImgMob()
-  
+
+          // Order conformation wrapper on Thank you page------------------------------------
+          var orderConformationWrapper = document.querySelector(".order-confirmation__wrapper");
+
+          // getting the transaction object--------------------------------------------------
+          var orderJSON = orderConformationWrapper.getAttribute("data-google-enhanced-transaction");
+
+          // Parsing the Stringfy JSON object-------------------
+          if (orderJSON) {
+              orderJSON = JSON.parse(orderJSON);
+              orderJSON.ecommerce.purchase.products.forEach(function (product) {
+                  if (product.name == "Single-Day Ticket") {
+                      pushOptimizelyEvents('SEA211_Product_sales_of_Single-Day_Ticket_product')
+                  }
+              })
+          }
       }
-  
-      /* Init variation */
-      helper.waitForElement(".homepage-page-header", init, 50, 25000);
-    } catch (e) {
+
+      /* Initialise variation */
+      // Only for /checkout/confirmation Page-------------------------
+      if (window.location.href.includes("/checkout/confirmation")) {
+          waitForElement(".order-confirmation__wrapper", init, 50, 15000);
+      }
+  } catch (e) {
       if (debug) console.log(e, "error in Test" + variation_name);
-    }
-  })();
+  }
+})();
