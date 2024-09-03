be128c03ef58ea5ce043584a168540b420600258
sarthakrautela <sarthakrautela80@gmail.com>
2024-09-03 13:18:38 +0530

    js

--- local_testing/Local1/variation/js.js
+++ local_testing/Local1/variation/js.js
@@ -1,64 +1,179 @@
 (function () {
-  try {
+    try {
       /* main variables */
       var debug = 1;
-      var variation_name = "SEA211-SWO";
-
-      function waitForElement(selector, trigger) {
-          var interval = setInterval(function () {
-              if (
-                  document &&
-                  document.querySelector(selector) &&
-                  document.querySelectorAll(selector).length > 0
-              ) {
-                  clearInterval(interval);
-                  trigger();
-              }
-          }, 50);
-          setTimeout(function () {
-              clearInterval(interval);
-          }, 15000);
-      }
-
-      // function for pushing the Optimixely events----------
-      function pushOptimizelyEvents(event) {
-          window['optimizely'] = window['optimizely'] || [];
-          window['optimizely'].push({
-              type: "event",
-              eventName: event,
-              tags: {
-                  revenue: 0, // Optional in cents as integer (500 == $5.00)
-                  value: 0.00 // Optional as float
-              }
-          });
-      }
-
-      /* Variation Init */
-      function init() {
-
-          // Order conformation wrapper on Thank you page------------------------------------
-          var orderConformationWrapper = document.querySelector(".order-confirmation__wrapper");
-
-          // getting the transaction object--------------------------------------------------
-          var orderJSON = orderConformationWrapper.getAttribute("data-google-enhanced-transaction");
-
-          // Parsing the Stringfy JSON object-------------------
-          if (orderJSON) {
-              orderJSON = JSON.parse(orderJSON);
-              orderJSON.ecommerce.purchase.products.forEach(function (product) {
-                  if (product.name == "Single-Day Ticket") {
-                      pushOptimizelyEvents('SEA211_Product_sales_of_Single-Day_Ticket_product')
-                  }
-              })
+      var variation_name = "cre-t-12";
+  
+  
+      function waitForElement(selector, trigger, delayInterval, delayTimeout) {
+        var interval = setInterval(function () {
+          if (
+            document &&
+            document.querySelector(selector) &&
+            document.querySelectorAll(selector).length > 0
+          ) {
+            clearInterval(interval);
+            trigger();
           }
+        }, delayInterval);
+        setTimeout(function () {
+          clearInterval(interval);
+        }, delayTimeout);
       }
-
-      /* Initialise variation */
-      // Only for /checkout/confirmation Page-------------------------
-      if (window.location.href.includes("/checkout/confirmation")) {
-          waitForElement(".order-confirmation__wrapper", init, 50, 15000);
+  
+      function live(selector, event, callback, context) {
+        function addEvent(el, type, handler) {
+          if (el.attachEvent) el.attachEvent("on" + type, handler);
+          else el.addEventListener(type, handler);
+        }
+        // matches polyfill
+        this.Element &&
+          (function (ElementPrototype) {
+            ElementPrototype.matches =
+              ElementPrototype.matches ||
+              ElementPrototype.matchesSelector ||
+              ElementPrototype.webkitMatchesSelector ||
+              ElementPrototype.msMatchesSelector ||
+              function (selector) {
+                var node = this,
+                  nodes = (node.parentNode || node.document).querySelectorAll(
+                    selector
+                  ),
+                  i = -1;
+                while (nodes[++i] && nodes[i] != node);
+                return !!nodes[i];
+              };
+          })(Element.prototype);
+  
+  
+        function live(selector, event, callback, context) {
+          addEvent(context || document, event, function (e) {
+            var found,
+              el = e.target || e.srcElement;
+            while (
+              el &&
+              el.matches &&
+              el !== context &&
+              !(found = el.matches(selector))
+            )
+              el = el.parentElement;
+            if (found) callback.call(el, e);
+          });
+        }
+        live(selector, event, callback, context);
       }
-  } catch (e) {
+  
+  
+      function addModal() {
+        var modalHtml = `
+            <div class="cre_12_container cre_12_hide">
+              <div class="cre_12_modal_container">
+                <div class="cre_12_modal_content">
+                  <div class="cre_12_modal_left">
+                    <h1 class="cre_12_title">
+                      STAY TUNED AMERICA!
+                      New Services Coming Soon...
+                    </h1>
+                    <p class="cre_12_paragraph">
+                      Be among the first in America to get GenderGP’s <b>quick and easy access to transition medication</b> and gender-affirming care.
+                    </p>
+                    <p class="cre_12_paragraph">
+                      Leave your email below and we’ll notify you the minute we open the doors.
+                    </p>
+                    <div class="cre_12_input_container">
+                      
+                    </div>
+                  </div>
+                  <div class="cre-t-12-stay-content-thank-you">
+                    <div class="cre-t-12-main-content-icon-thank-you">
+                        <img src="https://d27c6j8064skg9.cloudfront.net/ConversionRateExpert/GenderGP/test13/thank-you.svg" alt="thank you">
+                    </div>
+                    <div class="cre-t-12-main-content-2-thank-you">
+                        <h2 class="cre-t-12-main-content-header-thank-you">Thank You!</h2>
+                    </div>
+                    <div class="cre-t-12-main-content-3-thank-you">
+                        <p class="cre-t-12-main-content-subheader-thank-you">Your submission has been received!</p>
+                    </div>
+                  </div>
+                  <img class="cre_12_mobile_image" src="https://cdn-3.convertexperiments.com/uf/10007679/10007617/shutterstock_2200079503-min.jpg">
+                </div>
+                <span class="cre_12_close">
+                  <img src="https://cdn-3.convertexperiments.com/uf/10007679/10007617/gender-modal-close.png" alt="close icon" >
+                </span>
+              </div>
+            </div>
+        `;
+        if (!document.querySelector(".cre_12_container")) {
+          document.querySelector(".cre-t-12").insertAdjacentHTML("beforeend", modalHtml);
+        }
+      }
+  
+      function moveForm() {
+        var form = document.querySelector(".footer-widget #block-10");
+        console.log(form);
+        waitForElement(".cre_12_input_container", function () {
+  
+          document.querySelector(".cre_12_input_container").insertAdjacentElement("afterbegin", form);
+  
+        }, 50, 25000);
+      }
+  
+      function eventListeners() {
+  
+        if (window.innerWidth < 1150) {
+          setTimeout(() => {
+            var session = sessionStorage.getItem("cre_12_modal_triggered");
+            if (!session) {
+  
+              document.querySelector(".cre_12_container").classList.remove("cre_12_hide");
+              document.querySelector("body.cre-t-12").classList.add("cre_12_modal_triggered");
+            }
+          }, 24000);
+        }
+        else {
+  
+          document.body.addEventListener('mouseleave', function (event) {
+            var session = sessionStorage.getItem("cre_12_modal_triggered");
+            if (event.clientY <= 0 && !session && !document.querySelector(".cre_12_modal_triggered")) {
+              document.querySelector(".cre_12_container").classList.remove("cre_12_hide");
+              document.querySelector("body.cre-t-12").classList.add("cre_12_modal_triggered");
+            }
+          });
+        }
+  
+        live(".cre_12_container", "click", function (e) {
+          sessionStorage.setItem("cre_12_modal_triggered", true);
+          if (document.querySelector(".cre_12_modal_container").contains(e.target)) {
+            if (e.target === document.querySelector(".cre_12_close")) {
+              document.querySelector(".cre_12_container").classList.add("cre_12_hide");
+            }
+          }
+          else {
+            document.querySelector(".cre_12_container").classList.add("cre_12_hide");
+          }
+        });
+  
+      }
+  
+      function init() {
+        if (!document.body.classList.contains(variation_name)) {
+          document.body.classList.add(variation_name);
+  
+          addModal();
+          waitForElement(".footer-widget #block-10", moveForm, 25, 25000);
+  
+  
+            if (!window.cre_12_events) {
+            waitForElement(".cre_12_close", eventListeners, 50, 15000);
+            window.cre_12_events = true;
+          }
+  
+        }
+      }
+  
+      waitForElement("body", init, 50, 25000);
+  
+    } catch (e) {
       if (debug) console.log(e, "error in Test" + variation_name);
-  }
-})();
+    }
+  })();
