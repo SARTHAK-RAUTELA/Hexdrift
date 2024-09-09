546f30a3478abeb5585fd62d45305f8b2c92d0a5
sarthakrautela <sarthakrautela80@gmail.com>
2024-09-09 18:03:37 +0530

    js sfg 14

--- local_testing/Local2/variation/vB.js
+++ local_testing/Local2/variation/vB.js
@@ -2,7 +2,7 @@
   try {
     /* main variables */
     var debug = 1;
-    var variation_name = "cre-sfg-14";
+    var variation_name = "cre-t-17";
     /* helper library */
     var _$;
     !(function (factory) {
@@ -27,7 +27,7 @@
         },
         log: function () {
           var items = [];
-          for (var index = 0; index < arguments.length; index++) {
+          for (let index = 0; index < arguments.length; index++) {
             items.push(arguments[index]);
           }
           console && console.log(variation_name, items);
@@ -43,40 +43,6 @@
               }
             }
           });
-        },
-        live: function (selector, event, callback, context) {
-          /****Helper Functions****/
-          // helper for enabling IE 8 event bindings
-          function addEvent(el, type, handler) {
-            if (el.attachEvent) el.attachEvent("on" + type, handler);
-            else el.addEventListener(type, handler);
-          }
-          // matches polyfill
-          this.Element &&
-            (function (ElementPrototype) {
-              ElementPrototype.matches =
-                ElementPrototype.matches ||
-                ElementPrototype.matchesSelector ||
-                ElementPrototype.webkitMatchesSelector ||
-                ElementPrototype.msMatchesSelector ||
-                function (selector) {
-                  var node = this,
-                    nodes = (node.parentNode || node.document).querySelectorAll(selector),
-                    i = -1;
-                  while (nodes[++i] && nodes[i] != node);
-                  return !!nodes[i];
-                };
-            })(Element.prototype);
-          // live binding helper using matchesSelector
-          function live(selector, event, callback, context) {
-            addEvent(context || document, event, function (e) {
-              var found,
-                el = e.target || e.oldElement;
-              while (el && el.matches && el !== context && !(found = el.matches(selector))) el = el.parentElement;
-              if (found) callback.call(el, e);
-            });
-          }
-          live(selector, event, callback, context);
         },
         waitForElement: function (selector, trigger, delayInterval, delayTimeout) {
           var interval = setInterval(function () {
@@ -94,104 +60,154 @@
         return new bm(selector);
       };
     });
-    function waitForSwiper(trigger) {
-      var interval = setInterval(function () {
-        if (typeof window.Swiper != "undefined") {
-          clearInterval(interval);
-          trigger();
-        }
-      }, 50);
-      setTimeout(function () {
-        clearInterval(interval);
-      }, 15000);
-    }
-    // Helper functions
-    function addScript() {
-      var bmScript = document.createElement("script");
-      bmScript.src = "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js";
-      document.getElementsByTagName("head")[0].appendChild(bmScript);
-      var bmcsslink = document.createElement("link");
-      bmcsslink.rel = "stylesheet";
-      bmcsslink.href = "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css";
-      document.getElementsByTagName("head")[0].appendChild(bmcsslink);
-    }
-    var CreSlider = `
-<div class="cre-t-39-testimonial-wrapper" style="display:none">
-<div class="cre-t-39-testimonial-container ">
-<div class="cre_headinger">
-<h2>Introducing Facilities-iQ—our cutting-edge <br> facilities management software</h2>
-<p style="font-size: 16px;">Combining the <strong>latest SFG20 maintenance guidance</strong> with <strong> advanced facility management automation.</strong> <br> Facilities-iQ ensures unmatched efficiency, compliance, and peace of mind.</p>
-</div>
-<div class="cre-t-sfg14-boxslider swiper content-wrapper">
-<div class="cre-t-sfg14-boxslider-wrapper swiper-wrapper">
-<div class="cre-t-sfg14-box swiper-slide">
-<img src="https://cdn-3.convertexperiments.com/uf/10007679/10007638/Facilities-iQ%20Screenshots_%201%20Home%20Page.png">
-<p class="slider-caption-header" >Dashboard:</p>
-<p>With over 1000 regularly updated maintenance schedules, all aligned with the latest legislation, you can quickly find what you need using keywords, Uniclass, SFG20, or NRM codes.</p>
-</div>
-<div class="cre-t-sfg14-box swiper-slide">
-<img src="https://cdn-3.convertexperiments.com/uf/10007679/10007638/Facilities-iQ%20Screenshots_%207%20Tailoring%20A%20Schedule.png">
-<p class="slider-caption-header" >Maintenance Schedules:</p>
-<p>Each schedule contains clear task instructions along with task, timings, frequencies and colour-coded criticality.</p>
-</div>
-<div class="cre-t-sfg14-box swiper-slide">
-<img src="https://cdn-3.convertexperiments.com/uf/10007679/10007638/Facilities-iQ%20Screenshots_%202%20Technical%20Updates.png">
-<p class="slider-caption-header" >Technical Updates:</p>
-<p>SFG20 guidance is updated on a regular basis to ensure it always aligns with the latest legislation and codes of practice.</p>
-</div>
-<div class="cre-t-sfg14-box swiper-slide">
-<img src="https://cdn-3.convertexperiments.com/uf/10007679/10007638/Facilities-iQ%20Screenshots_%206%20Sharelink.png">
-<p class="slider-caption-header" >Share Link:</p>
-<p>Share your maintenance regimes securely for tendering or completion of works.</p>
-</div>
-</div>
-</div>
-<!-- If we need pagination -->
-<div class="cre-swiper-pagination"></div>
-</div>
-<!-- If we need navigation buttons -->
-<div class="cre-swiper-button-prev"></div>
-<div class="cre-swiper-button-next"></div>
-<!-- </div> -->
-<!-- </div> -->
-</div>
-`;
-    function intializeSwiper() {
-      var swiper = document.querySelector(".cre-t-sfg14-boxslider");
-      new Swiper(swiper, {
-        slidesPerView: 1.2,
-        centeredSlides: true,
-        grabCursor: true,
-        spaceBetween: 30,
-        loop: true,
-        speed: 800,  // transition duration in milliseconds
-        effect: 'slide',
-        pagination: {
-          el: ".cre-swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".cre-swiper-button-next",
-          prevEl: ".cre-swiper-button-prev",
-        },
-      });
-    }
-    var helper = _$();
-    function init() {
-      _$("body").addClass(variation_name);
-      if (!document.querySelector(".cre-t-39-testimonial-container")) {
-        document.querySelector(".body-container--home .widget-type-cell .row-number-1").insertAdjacentHTML("afterend", CreSlider);
+
+    function live(selector, event, callback, context) {
+      // helper for enabling IE 8 event bindings
+      function addEvent(el, type, handler) {
+          if (el.attachEvent) el.attachEvent("on" + type, handler);
+          else el.addEventListener(type, handler);
       }
-      waitForSwiper(
-        function () {
-          intializeSwiper();
-        },
-        50,
-        15000
-      );
+      // matches polyfill
+      this &&
+          this.Element &&
+          (function (ElementPrototype) {
+              ElementPrototype.matches =
+                  ElementPrototype.matches ||
+                  ElementPrototype.matchesSelector ||
+                  ElementPrototype.webkitMatchesSelector ||
+                  ElementPrototype.msMatchesSelector ||
+                  function (selector) {
+                      var node = this,
+                          nodes = (node.parentNode || node.document).querySelectorAll(selector),
+                          i = -1;
+                      while (nodes[++i] && nodes[i] != node);
+                      return !!nodes[i];
+                  };
+          })(Element.prototype);
+      // live binding helper using matchesSelector
+      function live(selector, event, callback, context) {
+          addEvent(context || document, event, function (e) {
+              var found,
+                  el = e.target || e.srcElement;
+              while (el && el.matches && el !== context && !(found = el.matches(selector))) el = el.parentElement;
+              if (found) callback.call(el, e);
+          });
+      }
+      live(selector, event, callback, context);
     }
-    addScript();
-    helper.waitForElement(".body-container--home .widget-type-cell .row-number-1", init, 50, 25000);
+
+
+    var helper = _$();
+
+
+
+
+function filedHide() {
+  var targetElement = document.querySelector('body');
+  if (targetElement) {
+    targetElement.classList.add('cre-t-17-filedHide')
+  }
+}
+
+function closeListener() {
+  live('.hs_submit .actions input.hs-button', 'click', function () {
+    var requiredFields = document.querySelectorAll('input[name="firstname"][required], input[name="lastname"][required],input[name="email"][required]');
+    var allFilled = true;
+    var targetElement = document.querySelector('body');
+    
+    // input[name="company"][required]
+
+    requiredFields.forEach(function (field) {
+      if (!field.value.trim() || field.classList.contains('error')) {
+        allFilled = false;
+      }
+    });
+
+    if (!allFilled) {
+      if (!targetElement.classList.contains('cre-t-17-filedHide')) {
+        targetElement.classList.add('cre-t-17-filedHide');
+      }
+    } else {
+      targetElement.classList.remove('cre-t-17-filedHide');
+      targetElement.classList.add('cre-t-17-error-handel');
+      
+    }
+  });
+}
+
+// This function for checking after user fill the first three fields 
+function closeListenerSec() {
+  live('.hs_submit .actions input.hs-button', 'click', function () {
+    var requiredFieldsSecStep = document.querySelectorAll('.input [name="company"], .input [name="jobtitle"],.input [inputmode="tel"]');
+    var allFilledSecStep = true;
+    var targetElement = document.querySelector('body');
+    
+    requiredFieldsSecStep.forEach(function (field) {
+      if (!field.value.trim() || field.classList.contains('error')) {
+        allFilledSecStep = false;
+      }
+    });
+
+    if (!allFilledSecStep) {
+      if (!targetElement.classList.contains('cre-t-17-filedHide2')) {
+        targetElement.classList.add('cre-t-17-filedHide2');
+      }
+    } else {
+      targetElement.classList.remove('cre-t-17-filedHide2');
+    }
+  });
+}
+
+var addNewContent = `<div class="cre-t-17-add-content-wrapper">
+<div class="cre-t-17-add-content-main">
+    <div class="cre-t-17-add-content-text">
+        The details below will help us tailor your demo to fit your specific needs. Rest assured, your data is safe with us and will never be shared.
+    </div>
+</div>
+</div>`
+
+function addNewContentText(){
+  helper.waitForElement('.form form .hs_company', function(){
+    document.querySelector('.form form .hs_company').insertAdjacentHTML('beforebegin', addNewContent)
+  }, 25, 50000)
+}
+
+    /* Variation Init */
+    function init() {
+      _$("body").addClass(variation_name); 
+      
+      filedHide()
+      closeListener()
+      addNewContentText()
+      closeListenerSec() 
+
+      helper.waitForElement('.cre-t-17-filedHide .hs_submit .actions input.hs-button', function(){
+        document.querySelector('.cre-t-17-filedHide .hs_submit .actions input.hs-button').value = 'REQUEST A DEMO';
+      },25, 50000)
+
+      live('.input [name="company"], .input [name="jobtitle"],.input [inputmode="tel"]', 'click', function(){
+        var targetElement = document.querySelector('body');
+        if (document.querySelector('.cre-t-17-error-handel')) {
+          targetElement.classList.remove('cre-t-17-error-handel');
+        }
+      })
+
+      // When user click CTA next time 
+      helper.waitForElement('.cre-t-17-filedHide2', function(){
+        if (document.querySelector('.cre-t-17-filedHide2')) {
+          live('.hs_submit .actions input.hs-button', 'click', function(){
+            var targetElement = document.querySelector('body');
+            if (document.querySelector('.cre-t-17-error-handel')) {
+              targetElement.classList.remove('cre-t-17-error-handel');
+              targetElement.classList.remove('cre-t-17-filedHide2');
+            }
+          })
+        }
+      },25, 25000)
+    }
+
+    /* Init variation */
+    helper.waitForElement(".form", init, 50, 25000);
   } catch (e) {
     if (debug) console.log(e, "error in Test" + variation_name);
   }
