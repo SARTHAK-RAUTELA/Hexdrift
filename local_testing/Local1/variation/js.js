12a570d7746d625fe8817d3405b5a77af58628de
sarthakrautela <sarthakrautela80@gmail.com>
2024-10-14 13:55:57 +0530

    css js

--- local_testing/Local1/variation/js.js
+++ local_testing/Local1/variation/js.js
@@ -1,24 +1,17 @@
-document.querySelectorAll('span').forEach(function(element){
-    if(element.textContent.includes('Available for a service call')){
-         {
-            const existingLi = [...(ul ? ul.querySelectorAll('li') : [])].some(li => li.textContent.includes('Available for a service call'));
-            if (!existingLi) {
-                createAndAppendLi('Available for a service call');
-            }
-    
-            // Add the hidetext class to the parent div of the first div containing the text
-           const selectedElement = card.querySelector('.dn.m_db.mt2');
-          const parentDiv = selectedElement ? selectedElement.closest('div') : null;
-    
-            if (parentDiv) {
-                parentDiv.classList.add('hidetext');
-            }
-        }
-    
-        })
 
         document.querySelectorAll('span').forEach(function(element){
             if(element.textContent.includes('Available for a service call')){
-                
+                const existingLi = [...(ul ? ul.querySelectorAll('li') : [])].some(li => li.textContent.includes('Available for a service call'));
+                if (!existingLi) {
+                    createAndAppendLi('Available for a service call');
+                }
+        
+                // Add the hidetext class to the parent div of the first div containing the text
+               const selectedElement = card.querySelector('.dn.m_db.mt2');
+              const parentDiv = selectedElement ? selectedElement.closest('div') : null;
+        
+                if (parentDiv) {
+                    parentDiv.classList.add('hidetext');
+                }
             }
         })
