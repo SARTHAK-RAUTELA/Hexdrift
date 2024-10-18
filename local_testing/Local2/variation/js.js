b70524ab7c5f1af81403fc3455648d770cc21d34
sarthakrautela <sarthakrautela80@gmail.com>
2024-10-18 12:13:18 +0530

    js

--- local_testing/Local2/variation/js.js
+++ local_testing/Local2/variation/js.js
@@ -1,60 +1,80 @@
-https://www.thumbtack.com/k/interior-designers/near-me?QA=tt157
-https://www.thumbtack.com/k/microwave-installation/near-me?QA=tt157
-https://www.thumbtack.com/k/ceiling-fan-install/near-me?QA=tt157
-https://www.thumbtack.com/k/dressmakers/near-me?QA=tt157
-https://www.thumbtack.com/k/leaf-removal/near-me?QA=tt157
-https://www.thumbtack.com/k/lawn-mower-repairs/near-me?QA=tt157
-https://www.thumbtack.com/k/sprinkler-systems-repair/near-me?QA=tt157
-https://www.thumbtack.com/k/bee-removal/near-me?QA=tt157
-https://www.thumbtack.com/k/wedding-bartendersl/near-me?QA=tt157
-https://www.thumbtack.com/k/kids-birthday-party-entertainersl/near-me?QA=tt157
-https://www.thumbtack.com/k/maidsl/near-me?QA=tt157
-https://www.thumbtack.com/k/mobile-cat-groome/near-me?QA=tt157
+(function () {
+	try {
+		/* main variables */
+		var debug = 0;
+		var variation_name = "TT-155";
+		/* all Pure helper functions */
+		function waitForElement(selector, trigger, delayInterval, delayTimeout) {
+			var interval = setInterval(function () {
+				if (document && document.querySelector(selector) && document.querySelectorAll(selector).length > 0) {
+					clearInterval(interval);
+					trigger();
+				}
+			}, delayInterval);
+			setTimeout(function () {
+				clearInterval(interval);
+			}, delayTimeout);
+		}
+	
+		
+	   
+		/* Variation Init */
+		function init() {
+			document.querySelector("body").classList.add(variation_name);
 
-https://www.thumbtack.com/k/affordable-maids/near-me?QA=tt157
-https://www.thumbtack.com/k/affordable-limousine-services/near-me?QA=tt157
-https://www.thumbtack.com/k/local-movers/near-me?QA=tt157
-https://www.thumbtack.com/k/swimming-pool-removal/near-me?QA=tt157
-https://www.thumbtack.com/k/hedge-trimming-services/near-me?QA=tt157
-https://www.thumbtack.com/k/porches/near-me?QA=tt157
-https://www.thumbtack.com/k/career-coach/near-me?QA=tt157
-https://www.thumbtack.com/k/interior-designers/near-me?QA=tt157
-https://www.thumbtack.com/k/microwave-installation/near-me?QA=tt157
-https://www.thumbtack.com/k/ceiling-fan-install/near-me?QA=tt157
-https://www.thumbtack.com/k/dressmakers/near-me?QA=tt157
-https://www.thumbtack.com/k/leaf-removal/near-me?QA=tt157
-https://www.thumbtack.com/k/lawn-mower-repairs/near-me?QA=tt157
-https://www.thumbtack.com/k/sprinkler-systems-repair/near-me?QA=tt157
-https://www.thumbtack.com/k/bee-removal/near-me?QA=tt157
-https://www.thumbtack.com/k/wedding-bartendersl/near-me?QA=tt157
-https://www.thumbtack.com/k/kids-birthday-party-entertainersl/near-me?QA=tt157
-https://www.thumbtack.com/k/maidsl/near-me?QA=tt157
-https://www.thumbtack.com/k/mobile-cat-groome?QA=tt157rs/near-me
-https://www.thumbtack.com/k/bathroom-remodeling/near-me?QA=tt157
-https://www.thumbtack.com/k/affordable-maids/near-me?QA=tt157
-https://www.thumbtack.com/k/affordable-limousine-services/near-me?QA=tt157
-https://www.thumbtack.com/k/local-movers/near-me?QA=tt157
-https://www.thumbtack.com/k/swimming-pool-removal/near-me?QA=tt157
-https://www.thumbtack.com/k/hedge-trimming-services/near-me?QA=tt157
-https://www.thumbtack.com/k/porches/near-me?QA=tt157
-https://www.thumbtack.com/k/career-coach/near-me?QA=tt157
-https://www.thumbtack.com/k/interior-designers/near-me?QA=tt157
-https://www.thumbtack.com/k/microwave-installation/near-me?QA=tt157
-https://www.thumbtack.com/k/ceiling-fan-install/near-me?QA=tt157
-https://www.thumbtack.com/k/dressmakers/near-me?QA=tt157
-https://www.thumbtack.com/k/leaf-removal/near-me?QA=tt157
-https://www.thumbtack.com/k/lawn-mower-repairs/near-me?QA=tt157
-https://www.thumbtack.com/k/sprinkler-systems-repair/near-me?QA=tt157
-https://www.thumbtack.com/k/bee-removal/near-me?QA=tt157
-https://www.thumbtack.com/k/wedding-bartendersl/near-me?QA=tt157
-https://www.thumbtack.com/k/kids-birthday-party-entertainersl/near-me?QA=tt157
-https://www.thumbtack.com/k/maidsl/near-me?QA=tt157
-https://www.thumbtack.com/k/mobile-cat-groome?QA=tt157rs/near-me?QA=tt157
-https://www.thumbtack.com/k/bathroom-remodeling/near-me?QA=tt157
-https://www.thumbtack.com/k/affordable-maids/near-me?QA=tt157
-https://www.thumbtack.com/k/affordable-limousine-services/near-me?QA=tt157
-https://www.thumbtack.com/k/local-movers/near-me?QA=tt157
-https://www.thumbtack.com/k/swimming-pool-removal/near-me?QA=tt157
-https://www.thumbtack.com/k/hedge-trimming-services/near-me?QA=tt157
-https://www.thumbtack.com/k/porches/near-me?QA=tt157
-https://www.thumbtack.com/k/career-coach/near-me?QA=tt157
+			if (document.querySelector('[aria-label="Breadcrumb"] ol li:last-child p')) {
+        var profession_content = document.querySelector(`[aria-label="Breadcrumb"] ol li:last-child p`).textContent;
+          
+       
+        if (window.location.href.startsWith("https://www.thumbtack.com/k/")) {
+          document.querySelector(`[class*="hero-header_filters"] [class*="Type_title3"]`).textContent =
+            "Hire a highly-rated " + profession_content + " pro with confidence.";
+        } else {
+          document.querySelector(`[class*="hero-header_filters"] [class*="Type_title3"]`).innerHTML =
+            "Hire a highly-rated " + profession_content + " pro with confidence.";
+        }
+        if (document.querySelector('[class*="hero-header_rootFlex"]>[class*=hero-header_heroHeaderExtendedHeight] picture+div h1')) {
+          document.querySelector('[class*="hero-header_rootFlex"]>[class*=hero-header_heroHeaderExtendedHeight] picture+div h1').innerHTML =
+            "Hire a highly-rated " + profession_content + " <span>pro with confidence.</span>";
+        }
+        if (document.querySelector('[class*="hero-header_heroHeaderHeight"] [class*="hero-header_flexHeaderContentTitle"]')) {
+          document.querySelector('[class*="hero-header_heroHeaderHeight"] [class*="hero-header_flexHeaderContentTitle"]').textContent =
+            "Hire a highly-rated " + profession_content + " pro with confidence.";
+        }
+        
+		  } 
+      document.querySelector('[class*="hero-header_filters"]  p').innerHTML = "Confirm your location and browse pros — you've got this.";
+       
+		   
+		   
+		  
+		 
+		}
+		
+		/* Initialise variation */
+		function thumbtackTest155(list, observer) {
+			list.getEntries().forEach((entry) => {
+				if (entry.entryType === "mark" && entry.name === "afterHydrate") {
+					observer.disconnect();
+					clearInterval(test144Interval);
+					waitForElement("body", init, 50, 15000);
+					window.isHydrated = true;
+				}
+			});
+		}
+		if (!window.isHydrated) {
+			var test144Interval = setInterval(function () {
+				waitForElement("body", init, 50, 15000);
+			}, 50);
+			setTimeout(function () {
+				clearInterval(test144Interval);
+			}, 3000);
+			const observer = new PerformanceObserver(thumbtackTest155);
+			observer.observe({ entryTypes: ["mark"] });
+		} else {
+			waitForElement("body", init, 50, 15000);
+		}
+	} catch (e) {
+		if (debug) console.log(e, "error in Test" + variation_name);
+	}
+  })();
