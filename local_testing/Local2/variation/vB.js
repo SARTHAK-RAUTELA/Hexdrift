31e94a055322eeb63c0d64a038ce7c3a38b9e79f
sarthakrautela <sarthakrautela80@gmail.com>
2024-11-26 11:35:56 +0530

    css

--- local_testing/Local2/variation/vB.js
+++ local_testing/Local2/variation/vB.js
@@ -1,98 +1,73 @@
 (function () {
-  try {
-    /* main variables */
-    var debug = 1;
-    var variation_name = "tt-160";
+	try {
+		/* main variables */
+		var debug = 1;
+		var variation_name = "tt-160";
 
-    function waitForElement(selector, trigger) {
-      var interval = setInterval(function () {
-        if (
-          document &&
-          document.querySelector(selector) &&
-          document.querySelectorAll(selector).length > 0
-        ) {
-          clearInterval(interval);
-          trigger();
-        }
-      }, 50);
-      setTimeout(function () {
-        clearInterval(interval);
-      }, 15000);
-    }
+		function waitForElement(selector, trigger) {
+			var interval = setInterval(function () {
+				if (
+					document &&
+					document.querySelector(selector) &&
+					document.querySelectorAll(selector).length > 0
+				) {
+					clearInterval(interval);
+					trigger();
+				}
+			}, 50);
+			setTimeout(function () {
+				clearInterval(interval);
+			}, 15000);
+		}
 
-    var ttSocialProof = `<tt-160-socialProof>
-        <div class="tt-160-socialProof-container">
-            <div class="tt-160-socialProof-wrapper">
+		var ttSocialProof = `<tt-160-socialProof>
+		<div class="tt-160-socialProof-container">
+			<div class="tt-160-socialProof-item-heading">
+					Featured on
+				</div>
+			<div class="tt-160-socialProof-wrapper">
 
-                <div class="tt-160-socialProof-item">
-                    Featured on
-                </div>
+			<div class="tt-160-socialProof-item">
+					Featured on
+				</div>
+				<div class="tt-160-socialProof-item">
+					<img src="https://cdn.optimizely.com/img/20611073899/c94e175d5b674673be2a6f1d7cab7b81.png" alt="tt-wired">
+				</div>
 
-                <div class="tt-160-socialProof-item">
-                    <img src="https://cdn.optimizely.com/img/20611073899/c94e175d5b674673be2a6f1d7cab7b81.png" alt="tt-wired">
-                </div>
+				<div class="tt-160-socialProof-item">
+					<img src="https://cdn.optimizely.com/img/20611073899/ff3eef077cda4b3ea3710044c42b2829.png" alt="tt-cbs-news">
+				</div>
 
-                <div class="tt-160-socialProof-item">
-                <img src="https://cdn.optimizely.com/img/20611073899/ff3eef077cda4b3ea3710044c42b2829.png" alt="tt-cbs-news">
-                </div>
+				<div class="tt-160-socialProof-item">
+					<img src="https://cdn.optimizely.com/img/20611073899/aa433c7999b140fbbf7c85b8295bde0c.png" alt="tt-forbes">
+				</div>
+				
+			</div>
+		</div>
+		</tt-160-socialProof>`
 
-                <div class="tt-160-socialProof-item">
-                    <img src="https://cdn.optimizely.com/img/20611073899/aa433c7999b140fbbf7c85b8295bde0c.png" alt="tt-forbes">
-                </div>
-            </div>
-        </div>
-    </tt-160-socialProof>`
+		function init() {
+        waitForElement("body",function(){
+			document.querySelector("body").classList.add(variation_name);
+		})
+			waitForElement("[class*='hero-header_root']+div", function () {
+				if (!document.querySelector("tt-160-socialProof")) {
+					document.querySelector("[class*='hero-header_root']+div").insertAdjacentHTML("beforebegin", ttSocialProof)
+				}
+			})
+		}
 
-    function init() {
-      waitForElement("body", function () {
-        document.querySelector("body").classList.add(variation_name);
-      })
+		init()
 
+		var tt160Interval = setInterval(function () {
+			init()
+		}, 100)
+		
+		setTimeout(function () {
+			clearInterval(tt160Interval)
+		}, 5000)
 
-      waitForElement("[class*='hero-header_filters']>div>div", function () {
-        if (!document.querySelector("[class*='hero-header_filters']>div>div + tt-160-socialProof")) {
-          document.querySelector("[class*='hero-header_filters']>div>div").insertAdjacentHTML("afterend", ttSocialProof)
-        }
-      })
-
-      // waitForElement("[class*='hero-header_heroContainer'] [class*='hero-header_heroHeaderHeight'] +div h1+div", function () {
-      //   if (!document.querySelector("[class*='hero-header_heroContainer'] [class*='hero-header_heroHeaderHeight'] +div h1+div + tt-160-socialProof")) {
-      //     document.querySelector("[class*='hero-header_heroContainer'] [class*='hero-header_heroHeaderHeight'] +div h1+div").insertAdjacentHTML("afterend", ttSocialProof)
-      //   }
-      // })
-
-      waitForElement("[class*='hero-header_heroContainer'] [class*='Wrap_root']", function () {
-        if (!document.querySelector("[class*='hero-header_heroContainer'] [class*='Wrap_root'] + tt-160-socialProof")) {
-          document.querySelector("[class*='hero-header_heroContainer'] [class*='Wrap_root']").insertAdjacentHTML("afterend", ttSocialProof)
-        }
-      })
-
-      // waitForElement('[class*="hero-header_flexHeaderContentTitle"]+div', function () {
-      //   console.log()
-      //   if (!document.querySelector('[class*="hero-header_flexHeaderContentTitle"]+div +tt-160-socialProof')) {
-      //     document.querySelector('[class*="hero-header_flexHeaderContentTitle"]+div').insertAdjacentHTML("afterend", ttSocialProof)
-      //   }
-      // })
-
-      waitForElement('[class*="hero-header_heroContainer"] [class*="hero-header_heroHeaderExtendedHeight"]+div', function () {
-        console.log()
-        if (!document.querySelector('[class*="hero-header_heroContainer"] [class*="hero-header_heroHeaderExtendedHeight"]+div +tt-160-socialProof')) {
-          document.querySelector('[class*="hero-header_heroContainer"] [class*="hero-header_heroHeaderExtendedHeight"]+div').insertAdjacentHTML("afterend", ttSocialProof)
-        }
-      })
-    }
-
-    init()
-
-    var tt160Interval = setInterval(function () {
-      init()
-    }, 100)
-
-    setTimeout(function () {
-      clearInterval(tt160Interval)
-    }, 5000)
-
-  } catch (e) {
-    if (debug) console.log(e, "error in Test" + variation_name);
-  }
+	} catch (e) {
+		if (debug) console.log(e, "error in Test" + variation_name);
+	}
 })();
