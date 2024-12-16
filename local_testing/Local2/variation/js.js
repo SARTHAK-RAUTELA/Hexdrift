6590ce2a636867c3ecc38f16c418188d3db51ed3
sarthakrautela <sarthakrautela80@gmail.com>
2024-12-16 13:04:45 +0530

    js

--- local_testing/Local2/variation/js.js
+++ local_testing/Local2/variation/js.js
@@ -1,49 +1,54 @@
-
 (function () {
-    try {
-      /* Main Variables */
-      const DEBUG = 1;
-      const VARIATION_NAME = "SEA241";
-      /* Utility Function: Wait for an Element */
-      function waitForElement(selector, callback, delayInterval, delayTimeout) {
-        const interval = setInterval(() => {
-          const element = document.querySelector(selector);
-          if (element) {
-            clearInterval(interval);
-            callback();
-          }
-        }, delayInterval);
-        setTimeout(() => clearInterval(interval), delayTimeout);
-      }
-     
-    
-      /* Initialize Popup */
-      function initPopup() {
-        document.body.classList.add(VARIATION_NAME);
-        const linkElement = document.querySelector('.site-header__links > div + a');
-
-        // Change the inner text
-        linkElement.innerHTML = 'VIP Tour';
-        
-        // Change the href attribute
-        linkElement.setAttribute('href', '/orlando/tours/ultimate-vip/?tab=overview');
-        
-        // Alternatively, you can do this:
-        linkElement.href = '/orlando/tours/ultimate-vip/?tab=overview';
-
-        
-     }
-
-      /* Initialize Variation */
-      waitForElement(
-        ".site-header__links > div + a",
-        () => {
-          initPopup();
-        },
-        50,
-        15000
-      );
-    } catch (e) {
-      if (DEBUG) console.error(`Error in Test ${VARIATION_NAME}:`, e);
+  try {
+    /* Main Variables */
+    const DEBUG = 1;
+    const VARIATION_NAME = "SEA241";
+    /* Utility Function: Wait for an Element */
+    function waitForElement(selector, callback, delayInterval, delayTimeout) {
+      const interval = setInterval(() => {
+        const element = document.querySelector(selector);
+        if (element) {
+          clearInterval(interval);
+          callback();
+        }
+      }, delayInterval);
+      setTimeout(() => clearInterval(interval), delayTimeout);
     }
-  })();
+   
+  
+    /* Initialize Popup */
+    function initPopup() {
+  document.body.classList.add(VARIATION_NAME);
+  
+      document.querySelector('.site-header__links > div + a').innerHTML = 'VIP Tour'
+
+  var linkElement = document.querySelector('.site-header__links > div + a');
+
+    
+      // Change the href attribute
+      linkElement.setAttribute('href', '/orlando/tours/ultimate-vip/?tab=overview');
+      
+   
+      if(document.querySelector('.site-header__mobile-links > [href="/orlando/vacation-packages"]')){
+          document.querySelector('.site-header__mobile-links > [href="/orlando/vacation-packages"]').innerHTML = 'VIP Tour';
+          document.querySelector('.site-header__mobile-links > [href="/orlando/vacation-packages"]').setAttribute('href', '/orlando/tours/ultimate-vip/?tab=overview');
+      
+      }
+
+
+      
+   }
+
+    /* Initialize Variation */
+    waitForElement(
+      ".site-header__links > div + a",
+      () => {
+        initPopup();
+      },
+      50,
+      15000
+    );
+  } catch (e) {
+    if (DEBUG) console.error(`Error in Test ${VARIATION_NAME}:`, e);
+  }
+})();
