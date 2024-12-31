f03d9dd72811195a88a732b762502d1395ed2471
sarthakrautela <sarthakrautela80@gmail.com>
2024-12-31 08:58:03 +0530

    css js

--- local_testing/Local2/variation/vB.js
+++ local_testing/Local2/variation/vB.js
@@ -1,139 +1,139 @@
 (function () {
   try {
-    /* main variables */
-    var debug = 1;
-    var variation_name = "Swf_06";
-    /* helper library */
-    var _$;
-    !(function (factory) {
-      _$ = factory();
-    })(function () {
-      var bm = function (s) {
-        if (typeof s === "string") {
-          this.value = Array.prototype.slice.call(document.querySelectorAll(s));
-        }
-        if (typeof s === "object") {
-          this.value = [s];
-        }
-      };
-      bm.prototype = {
-        eq: function (n) {
-          this.value = [this.value[n]];
-          return this;
-        },
-        each: function (fn) {
-          [].forEach.call(this.value, fn);
-          return this;
-        },
-        log: function () {
-          var items =[];
-          for (let index = 0; index < arguments.length; index++) {
-            items.push(arguments[index]);
-          }
-          console && console.log(variation_name, items);
-        },
-        addClass: function (v) {
-          var a = v.split(" ");
-          return this.each(function (i) {
-            for (var x = 0; x < a.length; x++) {
-              if (i.classList) {
-                i.classList.add(a[x]);
-              } else {
-                i.className += " " + a[x];
-              }
-            }
-          });
-        },
-        waitForElement: function (
-          selector,
-          trigger,
-          delayInterval,
-          delayTimeout
-        ) {
+      /* main variables */
+      var debug = 1;
+      var variation_name = "Swiftest_05";
+
+      function waitForElement(selector, trigger) {
           var interval = setInterval(function () {
-            if (_$(selector).value.length) {
-              clearInterval(interval);
-              trigger();
-            }
-          }, delayInterval);
+              if (
+                  document &&
+                  document.querySelector(selector) &&
+                  document.querySelectorAll(selector).length > 0
+              ) {
+                  clearInterval(interval);
+                  trigger();
+              }
+          }, 50);
           setTimeout(function () {
-            clearInterval(interval);
-          }, delayTimeout);
-        },
-      };
-      return function (selector) {
-        return new bm(selector);
-      };
-    });
-   
+              clearInterval(interval);
+          }, 15000);
+      }
 
-// Current date
-const currentDate = new Date();
+      function live(selector, event, callback, context) {
+          /****Helper Functions****/
+          // helper for enabling IE 8 event bindings
+          function addEvent(el, type, handler) {
+              if (el.attachEvent) el.attachEvent("on" + type, handler);
+              else el.addEventListener(type, handler);
+          }
+          // matches polyfill
+          this && this.Element &&
+              (function (ElementPrototype) {
+                  ElementPrototype.matches =
+                      ElementPrototype.matches ||
+                      ElementPrototype.matchesSelector ||
+                      ElementPrototype.webkitMatchesSelector ||
+                      ElementPrototype.msMatchesSelector ||
+                      function (selector) {
+                          var node = this,
+                              nodes = (
+                                  node.parentNode || node.document
+                              ).querySelectorAll(selector),
+                              i = -1;
+                          while (nodes[++i] && nodes[i] != node);
+                          return !!nodes[i];
+                      };
+              })(Element.prototype);
+          // live binding helper using matchesSelector
+          function live(selector, event, callback, context) {
+              addEvent(context || document, event, function (e) {
+                  var found,
+                      el = e.target || e.srcElement;
+                  while (
+                      el &&
+                      el.matches &&
+                      el !== context &&
+                      !(found = el.matches(selector))
+                  )
+                      el = el.parentElement;
+                  if (found) callback.call(el, e);
+              });
+          }
+          live(selector, event, callback, context);
+      }
 
-// Subtract 2 days
-const twoDaysBefore = new Date();
-twoDaysBefore.setDate(currentDate.getDate() - 2);
+      function debounce(func, timeout = 300) {
+          let timer;
+          return (...args) => {
+              clearTimeout(timer);
+              timer = setTimeout(() => {
+                  func.apply(this, args);
+              }, timeout);
+          };
+      }
 
-// Format the date (e.g., DD Month YYYY)
-const options = { day: '2-digit', month: 'long', year: 'numeric' };
-const formattedDate = twoDaysBefore.toLocaleDateString('en-GB', options); 
+      function scrollToEl(el) {
+        var scrollHeight = document.querySelector(el)
+        scrollHeight = scrollHeight && scrollHeight.getBoundingClientRect().top || 0;
+        scrollHeight = scrollHeight + scrollY - 50;
+        scrollTo({ top: scrollHeight, behavior: "smooth" });
+      }
 
-// Update the HTML string with the formatted date
-var TheSwiftest_06 = `
-<div class="swf_section">
-    <div class="swf_container">
-        <div class="swf_times">
-            <img src="https://cdn-3.convertexperiments.com/uf/10007679/10007714/time-logo_676504cc408c2.svg" alt="">
-            <p class="times_content">Last updated:<strong> ${formattedDate}</strong></p>
-        </div>
-        <div class="swf_content">
-            <h2 class="swf_heading">2024’s <span class="border">Best Pet Insurance Plans</span></h2>
-            <p class="swf_subheading"><strong>Your furry friend deserves the very best care.</strong> Trusted by over
-                10,000 pet owners since 2020, these plans deliver the coverage you need, at great value.</p>
-        </div>
-        <ul class="swf_list">
-            <li class="swf_item">
-                <div class="check_mark">
-                    <img src="https://cdn-3.convertexperiments.com/uf/10007679/10007714/check-mark_676504a22a13f.svg" alt="">
-                </div>
-                <p class="bulet_content"><strong>Top brands</strong> offering excellent service and fast claims payouts</p>
-            </li>
-            <li class="swf_item">
-                <div class="check_mark">
-                    <img src="https://cdn-3.convertexperiments.com/uf/10007679/10007714/check-mark_676504a22a13f.svg" alt="">
-                </div>
-                <p class="bulet_content"><strong>Guaranteed lowest prices</strong> with exclusive deals</p>
-            </li>
-            <li class="swf_item">
-                <div class="check_mark">
-                    <img src="https://cdn-3.convertexperiments.com/uf/10007679/10007714/check-mark_676504a22a13f.svg" alt="">
-                </div>
-                <p class="bulet_content"><strong>Accepted by all vets</strong> across the United States</p>
-            </li>
-        </ul>
-    </div>
-</div>`;
+      function checkElemntPosition() {
+          var element = document.querySelector(".inside-article .entry-content>div:last-of-type")
+          if (element) {
 
-    var helper = _$();
-    /* Variation Init */
-    function init() {
-      _$('body').addClass(variation_name)
-      helper.waitForElement('#page .inside-article [id*="modal"] + div .gb-inside-container', function () {
-        if (!document.querySelector(".swf_section")) {
-            document.querySelector("#page .inside-article [id*='modal'] + div .gb-inside-container").innerHTML = (TheSwiftest_06 )
+              if (element.getClientRects()[0].top <= 70) {
+                  document.querySelector("body").classList.add("cre-t-05-showStickyButton")
+              } else {
+                  document.querySelector("body.cre-t-05-showStickyButton")?.classList.remove("cre-t-05-showStickyButton")
+              }
+          }
+      }
+
+      var swiftest_swf05 = `
+      <div class="swiftest_05_sticky_footer" style="display:none;">
+          <button type='button' class="Sticky_cta">VIEW PLANS</button>
+      </div>`;
+
+      function eventHandler() {
+
+          document.addEventListener("scroll", function () {
+              var checkForSticky = debounce(checkElemntPosition)
+              checkForSticky()
+          })
+
+          live(".swiftest_05_sticky_footer .Sticky_cta", "click", function () {
+              scrollToEl('.gb-container [data-before="1"]');
+          })
+
+      }
+
+      /* Variation Init */
+      function init() {
+        if(window.innerWidth <=768){
+            document.querySelector("body").classList.add(variation_name);
+          if (!document.querySelector(".swiftest_05_sticky_footer")) {
+              document.querySelector("body").insertAdjacentHTML("beforeend", swiftest_swf05)
+          }
+
+          waitForElement(".swiftest_05_sticky_footer", function () {
+              checkElemntPosition()
+          })
+
+          if (!window.cre_t_05_eventHandler) {
+              eventHandler()
+              window.cre_t_05_eventHandler = true;
+          }
         }
+          
 
-
-    }, 50, 15000)
-    helper.waitForElement('#page .inside-article [id*="modal"] + .gb-grid-wrapper .wp-block-image', function () {
-            document.querySelector("#page .inside-article [id*='modal'] + .gb-grid-wrapper .wp-block-image").innerHTML=`<img src="https://cdn-3.convertexperiments.com/uf/10007679/10007714/dogcat-img_676504b20e461.svg">`
-    }, 50, 15000)
-   
-    
-    }
-    /* Initialise variation */
-    helper.waitForElement("body", init, 50, 15000);
+      }
+      
+      /* Initialise variation */
+      waitForElement("body", init)
   } catch (e) {
-    if (debug) console.log(e, "error in Test" + variation_name);
+      if (debug) console.log(e, "error in Test" + variation_name);
   }
 })();
