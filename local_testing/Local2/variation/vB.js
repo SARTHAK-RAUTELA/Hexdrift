af843e7b73d07f5b8b0ff54bc9e48cb95a2f8384
sarthakrautela <sarthakrautela80@gmail.com>
2025-02-19 12:31:14 +0530

    css js

--- local_testing/Local2/variation/vB.js
+++ local_testing/Local2/variation/vB.js
@@ -1,20 +1,35 @@
-https://www.thumbtack.com/k/deep-cleaning/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-298759174068-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&Test_164=True
-
-https://www.thumbtack.com/k/deep-cleaning/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-969844309891-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&Test_164=True
-
-https://www.thumbtack.com/k/deep-cleaning/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-295778204526-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&Test_164=True
-
-https://www.thumbtack.com/k/handyman/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-328405356611-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&Test_164=True
-
-https://www.thumbtack.com/k/handyman/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-218909059684-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&Test_164=True
-
-https://www.thumbtack.com/k/handyman/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-289878774-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&Test_164=True
-
-https://www.thumbtack.com/k/24-hour-plumbers/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-50198310-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&Test_164=True
-
-https://www.thumbtack.com/k/24-hour-plumbers/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-116741836-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&Test_164=True
-https://www.thumbtack.com/k/emergency-electricians/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-791986862-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&Test_164=True
-
-https://www.thumbtack.com/k/emergency-electricians/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-1067756062-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&Test_164=True
-
-https://www.thumbtack.com/k/emergency-electricians/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-4568059488-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&Test_164=True
+(function () {
+    try {
+      /* Main Variables */
+      const VARIATION_NAME = "TT_164";
+  
+      /* Utility Function: Wait for an Element */
+      function waitForElement(selector, callback, delayInterval, delayTimeout) {
+        const interval = setInterval(() => {
+          const element = document.querySelector(selector);
+          if (element) {
+            clearInterval(interval);
+            callback();
+          }
+        }, delayInterval);
+  
+        setTimeout(() => {
+          clearInterval(interval);
+        }, delayTimeout);
+      }
+  
+      /* Initialize Popup */
+      function init() {
+        document.body.classList.add(VARIATION_NAME);
+  
+      }
+  
+      /* Initialize Variation */
+      waitForElement("body", () => {
+        init();
+      }, 50, 15000);
+  
+    } catch (e) {
+      if (DEBUG) console.error(`Error in Test ${VARIATION_NAME}:`, e);
+    }
+  })();
