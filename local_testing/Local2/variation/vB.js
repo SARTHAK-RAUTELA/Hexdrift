ffc271c56866a17edc1bb73ac404354fa9c114a3
sarthakrautela <sarthakrautela80@gmail.com>
2025-02-25 16:17:06 +0530

    js

--- local_testing/Local2/variation/vB.js
+++ local_testing/Local2/variation/vB.js
@@ -1,230 +1,216 @@
 (function () {
-  try {
-    /* main variables */
-    var debug = 1;
-    var variation_name = "cre_ggp_br";
-
-    function waitForElement(selector, trigger, delayInterval, delayTimeout) {
-      var interval = setInterval(function () {
-        if (
-          document &&
-          document.querySelector(selector) &&
-          document.querySelectorAll(selector).length > 0
-        ) {
-          clearInterval(interval);
-          trigger();
-        }
-      }, delayInterval);
-      setTimeout(function () {
-        clearInterval(interval);
-      }, delayTimeout);
-    }
-
-    function live(selector, event, callback, context) {
-      function addEvent(el, type, handler) {
-        if (el.attachEvent) el.attachEvent("on" + type, handler);
-        else el.addEventListener(type, handler);
-      }
-
-      // matches polyfill
-      this.Element &&
-        (function (ElementPrototype) {
-          ElementPrototype.matches =
-            ElementPrototype.matches ||
-            ElementPrototype.matchesSelector ||
-            ElementPrototype.webkitMatchesSelector ||
-            ElementPrototype.msMatchesSelector ||
-            function (selector) {
-              var node = this,
-                nodes = (node.parentNode || node.document).querySelectorAll(selector),
-                i = -1;
-              while (nodes[++i] && nodes[i] != node);
-              return !!nodes[i];
+    try {
+        /* Main Variables */
+        var debug = 1;
+        var variation_name = "cre-Cambridge_8V2";
+        /* Helper Library */
+        var _$;
+        !(function (factory) {
+            _$ = factory();
+        })(function () {
+            var bm = function (s) {
+                if (typeof s === "string") {
+                    this.value = Array.prototype.slice.call(document.querySelectorAll(s));
+                }
+                if (typeof s === "object") {
+                    this.value = [s];
+                }
             };
-        })(Element.prototype);
-
-      function live(selector, event, callback, context) {
-        addEvent(context || document, event, function (e) {
-          var found,
-            el = e.target || e.srcElement;
-          while (el && el.matches && el !== context && !(found = el.matches(selector))) el = el.parentElement;
-          if (found) callback.call(el, e);
+            bm.prototype = {
+                eq: function (n) {
+                    this.value = [this.value[n]];
+                    return this;
+                },
+                each: function (fn) {
+                    [].forEach.call(this.value, fn);
+                    return this;
+                },
+                log: function () {
+                    var items = [];
+                    for (let index = 0; index < arguments.length; index++) {
+                        items.push(arguments[index]);
+                    }
+                    console && console.log(variation_name, items);
+                },
+                addClass: function (v) {
+                    var a = v.split(" ");
+                    return this.each(function (i) {
+                        for (var x = 0; x < a.length; x++) {
+                            if (i.classList) {
+                                i.classList.add(a[x]);
+                            } else {
+                                i.className += " " + a[x];
+                            }
+                        }
+                    });
+                },
+                waitForElement: function (selector, trigger, delayInterval, delayTimeout) {
+                    var interval = setInterval(function () {
+                        if (_$(selector).value.length) {
+                            clearInterval(interval);
+                            trigger(document.querySelector(selector));
+                        }
+                    }, delayInterval);
+                    setTimeout(function () {
+                        clearInterval(interval);
+                    }, delayTimeout);
+                },
+                live: function (selector, event, callback, context) {
+                    function addEvent(el, type, handler) {
+                        if (el.attachEvent) el.attachEvent("on" + type, handler);
+                        else el.addEventListener(type, handler);
+                    }
+                    function live(selector, event, callback, context) {
+                        addEvent(context || document, event, function (e) {
+                            var found,
+                                el = e.target || e.srcElement;
+                            while (el && el.matches && el !== context && !(found = el.matches(selector))) 
+                                el = el.parentElement;
+                            if (found) callback.call(el, e);
+                        });
+                    }
+                    live(selector, event, callback, context);
+                },
+            };
+            return function (selector) {
+                return new bm(selector);
+            };
         });
-      }
-      live(selector, event, callback, context);
-    }
-
-    function addModal() {
-      var modalHtml = `
-            <div class="cre_ggp_br_container cre_ggp_br_hide">
-              <div class="cre_ggp_br_modal_container">
-                <div class="cre_ggp_br_modal_content">
-                  <div class="cre_ggp_br_modal_left">
-                    <h1 class="cre_ggp_br_title">
-                      STAY TUNED BRAZIL!
-                      New Services Coming Soon...
-                    </h1>
-                    <p class="cre_ggp_br_paragraph">
-                      Be among the first in Brazil to get GenderGP’s <b>quick and easy access to transition medication</b> and gender-affirming care.
-                    </p>
-                    <p class="cre_ggp_br_paragraph">
-                      Leave your email below and we’ll notify you the minute we open the doors.
-                    </p>
-                    <div class="cre_ggp_br_input_container"></div>
-                  </div>
-                  <div class="cre_ggp_br-stay-content-thank-you">
-                    <div class="cre_ggp_br-main-content-icon-thank-you">
-                        <img src="https://d27c6j8064skg9.cloudfront.net/ConversionRateExpert/GenderGP/test13/thank-you.svg" alt="thank you">
-                    </div>
-                    <div class="cre_ggp_br-main-content-2-thank-you">
-                        <h2 class="cre_ggp_br-main-content-header-thank-you">Thank You!</h2>
-                    </div>
-                    <div class="cre_ggp_br-main-content-3-thank-you">
-                        <p class="cre_ggp_br-main-content-subheader-thank-you">Your submission has been received!</p>
-                    </div>
-                  </div>
-                  <img class="cre_ggp_br_mobile_image" src="https://cdn-3.convertexperiments.com/uf/10007679/10007617/shutterstock_2200079503-min.jpg">
-                </div>
-                <span class="cre_ggp_br_close">
-                  <img src="https://cdn-3.convertexperiments.com/uf/10007679/10007617/gender-modal-close.png" alt="close icon">
-                </span>
-              </div>
-            </div>
-        `;
-      if (!document.querySelector(".cre_ggp_br_container")) {
-        document.querySelector(".cre_ggp_br").insertAdjacentHTML("beforeend", modalHtml);
-      }
-    }
-
-
-    function changeJourneyPlaceholder() {
-      const journeySelect = document.querySelector('[cre-test-id="brazil-sitewide"] select[name="journey"]');
-      if (journeySelect) {
-        journeySelect.options[0].text = "What journey are you on? (Select)";
-      }
-    }
-
-    function moveForm() {
-      var form = document.querySelector('[cre-test-id="brazil-sitewide"]');
-
-      waitForElement(".cre_ggp_br_input_container", function () {
-        form.querySelector('input[type="email"]').setAttribute("placeholder", "Email*");
-        document.querySelector(".cre_ggp_br_input_container").insertAdjacentElement("afterbegin", form);
-
-        // Handle form submission
-        // form.addEventListener("submit", function (event) {
-        //   waitForElement(".cre_ggp_br_container", function () {
-        //     document.body.classList.add("signUpForm-submitted");
-        //     document.body.classList.add("cre_ggp_br-submitted");
-
-        //   }, 50, 5000);
-        // });
-
-
-      }, 50, 25000);
-    }
-
-    function setTimer() {
-      var now = new Date().getTime();
-      sessionStorage.setItem('cre_ggp_br_user_entered', now);
-    }
-
-
-    function mobileModalTrigger() {
-      var modalInterval = setInterval(() => {
-        var session = sessionStorage.getItem("cre_ggp_br_modal_triggered");
-        if (!session) {
-          var userEntry = sessionStorage.getItem('cre_ggp_br_user_entered');
-
-          if (userEntry != null) {
-            var now = new Date();
-            var startTime = parseInt(userEntry);
-            if (startTime + 24000 < now.getTime()) {
-
-              setTimeout(function () {
-                waitForElement('[cre-test-id="brazil-sitewide"] form', moveForm, 25, 25000);
-              }, 1000);
-              document.querySelector(".cre_ggp_br_container").classList.remove("cre_ggp_br_hide");
-              document.querySelector("body.cre_ggp_br").classList.add("cre_ggp_br_modal_triggered");
-
-              clearInterval(modalInterval);
+        var helper = _$();
+    
+          
+            
+            helper.live('.Cre_application', 'click', function () {
+                document.body.classList.add('hidestup4');
+                
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+                helper.waitForElement('.wpb_wrapper > #gform_wrapper_71', function () {
+                    // Check if the element with 75% width exists
+                    helper.waitForElement('.gf_progressbar_percentage[style="width:75%;"]', function() {
+                       
+                    let progressBar = document.querySelector('.gf_progressbar_percentage[style="width:75%;"]');
+                    if (progressBar) {
+                        updatePopupContent();// Add class if progress bar is 75%
+                    }
+                  }, 50, 5000); 
+                  
+                    
+                }, 500, 15000);
+            });
+            helper.live('.CrePrevious', 'click', function () {
+                document.body.classList.remove('hidestup4');
+            
+                // Check if the progress bar with 75% width is NOT present
+                helper.waitForElement('.gf_progressbar_percentage[style="width:75%;"]', function() {
+                if (document.querySelector('.gf_progressbar_percentage[style="width:75%;"]')) {
+                    document.body.classList.remove('Cre_showcta');
+                }
+              }, 50, 5000);
+            
+                helper.waitForElement('#gform_previous_button_71', function (prevButton) {
+                    prevButton.click();
+                }, 50, 5000);
+            });
+            helper.live('.Cre_price', 'click', function () {
+                document.querySelector('.cre_cta').insertAdjacentHTML(
+                    'beforeend',
+                    '<img id="gform_ajax_spinner_71" class="gform_ajax_spinner cre_loader" src="https://www.reachcambridge.com/wp-content/plugins/gravityforms/images/spinner.svg" alt="">'
+                );
+                sessionStorage.setItem('redirecting', 'true');
+                helper.waitForElement('#gform_submit_button_71', function (submitButton) {
+                    const form = submitButton.closest('form#gform_71');
+                    if (form) {
+                        form.submit();
+                    } else {
+                        submitButton.style.display = 'block';
+                        submitButton.click();
+                        submitButton.style.display = 'none';
+                    
+                    }
+                }, 50, 5000);
+            });
+            helper.live('.Cre_goaccount', 'click', function () {
+                document.body.classList.remove('hidestup4');
+            });
+            helper.live('#gform_next_button_71_51', 'click', function () {
+                helper.waitForElement('.wpb_wrapper > #gform_wrapper_71', function () {
+                    // Check if the element with 75% width exists
+                    helper.waitForElement('.gf_progressbar_percentage[style="width:75%;"]', function() {
+                       
+                    let progressBar = document.querySelector('.gf_progressbar_percentage[style="width:75%;"]');
+                    if (progressBar) {
+                        updatePopupContent();// Add class if progress bar is 75%
+                    }
+                  }, 50, 5000); 
+                  
+                    
+                }, 500, 15000);
+            });
+        
+        function updatePopupContent() {
+            const nameElement = document.querySelector('.apply-right-course.first');
+            const courseDateElement = document.querySelector('#sidebar .apply-right-programs .apply-right-title');
+            const priceElement = document.querySelector('.apply-right-fees .apply-right-total');
+            if (!nameElement || !courseDateElement || !priceElement) {
+                return;
             }
-          }
-          else {
-            setTimer();
-          }
+            const name = nameElement.textContent.trim() || 'Student';
+            const courseDate = courseDateElement.textContent.trim() || 'your selected date';
+            const price = priceElement.textContent.trim() || 'Price not available';
+            const popupHTML = `
+                <div class="cre_popupshow">
+                    <div class="Ctacontroll">
+                        <p class='CrePrevious'>Previous</p>
+                        <p class="Cre_application">Next</p>
+                    </div>
+                    <div class="Cre_popupinside">
+                        <h2>Pay your deposit</h2>
+                        <p>Please pay now to guarantee ${name}’s course and ${courseDate}.</p>
+                        <div class="cre_cta">
+                            <span class="Cre_price">Pay Now (${price})</span>
+                            <span class="Cre_goaccount">Request to Pay Later</span>
+                        </div>
+                    </div>
+                </div>
+            `;
+            const progressBar = document.querySelector('#gform_page_71_3[style="display:none;"]  + #gform_page_71_4');
+            const existingPopup = document.querySelector('.cre_popupshow');
+            if (existingPopup) {
+                existingPopup.outerHTML = popupHTML;
+            } else if (progressBar) {
+                helper.waitForElement('#gform_page_71_3[style="display:none;"]  + #gform_page_71_4', function() {
+                       
+                    let progressBar = document.querySelector('#gform_page_71_3[style="display:none;"]  + #gform_page_71_4');
+                    if (progressBar) {
+                        progressBar.insertAdjacentHTML("afterend", popupHTML);
+                    }
+                  }, 50, 5000); 
+                
+            }
         }
-
-      }, 50);
+        function init() {
+            _$('body').addClass(variation_name);
+            function checkURLAndRedirect() {
+                if (sessionStorage.getItem('redirecting') === 'true') {
+                    const checkInterval = setInterval(() => {
+                        if (window.location.href.includes('dashboard?reachFormEvent=Reach')) {
+                            clearInterval(checkInterval);
+                            document.querySelector('.page-template-dashboard').style.opacity = '0';
+                            document.querySelector('.nectar-button').click();
+                            window.location.href = 'https://www.reachcambridge.com/checkout';
+                        }
+                    }, 100);
+                    setTimeout(() => clearInterval(checkInterval), 30000);
+                }
+            }
+            checkURLAndRedirect();
+        }
+        // if (!window.goalclickAdded205) {
+        //     window.goalclickAdded205 = true;
+        //     eventHandlerT8();
+        // }
+        helper.waitForElement("body", init, 50, 5000);
+    } catch (e) {
+        console.log(e, "Error in Test " + variation_name);
     }
-
-    // function setTimer() {
-    //   var now = new Date().getTime();
-    //   sessionStorage.setItem('cre_ggp_br_user_entered', now);
-    // }
-
-    function eventListeners() {
-
-      if (window.innerWidth < 1150) {
-        mobileModalTrigger();
-
-      }
-      else {
-
-        document.body.addEventListener('mouseleave', function (event) {
-          var session = sessionStorage.getItem("cre_ggp_br_modal_triggered");
-          if (event.clientY <= 0 && !session && !document.querySelector(".cre_ggp_br_modal_triggered")) {
-            waitForElement('[cre-test-id="brazil-sitewide"] form', moveForm, 25, 25000);
-            document.querySelector(".cre_ggp_br_container").classList.remove("cre_ggp_br_hide");
-            document.querySelector("body.cre_ggp_br").classList.add("cre_ggp_br_modal_triggered");
-          }
-        });
-      }
-
-      live(".cre_ggp_br_container", "click", function (e) {
-        sessionStorage.setItem("cre_ggp_br_modal_triggered", true);
-        if (document.querySelector(".cre_ggp_br_modal_container").contains(e.target)) {
-          if (e.target === document.querySelector(".cre_ggp_br_close")) {
-            document.querySelector(".cre_ggp_br_container").classList.add("cre_ggp_br_hide");
-          }
-        }
-        else {
-          document.querySelector(".cre_ggp_br_container").classList.add("cre_ggp_br_hide");
-        }
-      });
-
-
-      var wpcf7Elm = document.querySelector('[cre-test-id="brazil-sitewide"] .wpcf7-form');
-      wpcf7Elm && wpcf7Elm.addEventListener('wpcf7submit', function (event) {
-        console.log("Form coming braz il")
-        setTimeout(() => {
-          if (document.querySelector('[cre-test-id="brazil-sitewide"] .wpcf7-form.sent')) {
-            document.body.classList.add('signUpForm-submitted')
-            document.body.classList.add("cre_ggp_br-submitted");
-          }
-        }, 3000);
-      }, false);
-
-    }
-
-    function init() {
-      if (!document.body.classList.contains(variation_name)) {
-        document.body.classList.add(variation_name);
-        addModal();
-        // waitForElement('[cre-test-id="brazil-sitewide"] select[name="journey"]', changeJourneyPlaceholder, 50, 15000);
-        // waitForElement(".cre_ggp_br_close", eventListeners, 50, 15000);
-
-        if (!window.cre_br_events) {
-          waitForElement('[cre-test-id="brazil-sitewide"] select[name="journey"]', changeJourneyPlaceholder, 50, 15000);
-          waitForElement(".cre_ggp_br_close", eventListeners, 50, 15000);
-          window.cre_br_events = true;
-        }
-
-      }
-    }
-
-    waitForElement("body", init, 50, 25000);
-  } catch (e) {
-    if (debug) console.log(e, "error in Test" + variation_name);
-  }
-})();
+  })();
