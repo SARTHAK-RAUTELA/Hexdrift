4e084b3f31f2c3ed3ecb72ba7ce20b8381f4b596
sarthakrautela <sarthakrautela80@gmail.com>
2025-03-26 12:38:51 +0530

    css js

--- local_testing/Local2/variation/vB.js
+++ local_testing/Local2/variation/vB.js
@@ -1,20 +1,32 @@
-https://www.thumbtack.com/k/deep-cleaning/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-298759174068-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&QA=tt166
-
-https://www.thumbtack.com/k/deep-cleaning/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-969844309891-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&QA=tt166
-
-https://www.thumbtack.com/k/deep-cleaning/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-295778204526-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&QA=tt166
-
-https://www.thumbtack.com/k/handyman/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-328405356611-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&QA=tt166
-
-https://www.thumbtack.com/k/handyman/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-218909059684-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&QA=tt166
-
-https://www.thumbtack.com/k/handyman/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-289878774-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&QA=tt166
-
-https://www.thumbtack.com/k/24-hour-plumbers/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-50198310-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&QA=tt166
-
-https://www.thumbtack.com/k/24-hour-plumbers/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-116741836-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&QA=tt166
-https://www.thumbtack.com/k/emergency-electricians/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-791986862-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&QA=tt166
-
-https://www.thumbtack.com/k/emergency-electricians/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-1067756062-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&QA=tt166
-
-https://www.thumbtack.com/k/emergency-electricians/near-me?utm_medium=cpc&utm_source=cma-google&utm_campaign=s-c-1664689605-59670161890-321162491176-kwd-4568059488-b&gclsrc=aw.ds&gad_source=1&~campaign_id=1664689605&%243p=a_google_adwords&feature=paid%20advertising&gclid=Cj0KCQjwgrO4BhC2ARIsAKQ7zUm_V5kJS_Lk36Zf6mSu7Xm9XsEETPaqb3dBlX3ZIxDbGKbSq8tfkSgaAt&QA=tt166
+window.expLibraryDataQueue = window.expLibraryDataQueue || [];
+window.expLibraryDataQueue.push({
+  CRE_EXP_47: {
+    var: {
+      bannerHTML: `
+            <div class="Cre_banner_logo jeewan" style="display:none">
+          <img src="https://d27c6j8064skg9.cloudfront.net/ConversionRateExpert/TravelShift/CS2/Test47/best-price.png" width="300" height="100" 
+          alt="Best Price">
+          <img src="https://d27c6j8064skg9.cloudfront.net/ConversionRateExpert/TravelShift/CS2/Test47/travel-agency.png" width="300" height="100" 
+          alt="Travel-agency">
+        </div>`
+    },
+    enable_DEBUG: true,
+    initOnce: false,
+    localDevelopment: true,
+    // Initialize the experiment
+    init() {
+      let $this = this;
+      this.runAt('html[lang="en"] body[data-path="/book-trips-holiday"] [class*="FrontCover_headerWrapper"] h1 + div', function ([element]) {
+        if (!document.querySelector(".Cre_banner_logo")) {
+          element.insertAdjacentHTML('afterend', $this.var.bannerHTML)
+        }
+      })
+      this.runAt('html[lang="en"] body[data-path="/book-trips-holiday"] #frontCover  [class*="FrontCover_coverImageWrapper"] + div [class*="FrontCover_headerWrapper"] [class*="FrontCover_description"]', function ([element]) {
+         if (!document.querySelector(' #frontCover  [class*="FrontCover_coverImageWrapper"] + div [class*="FrontCover_headerWrapper"] [class*="FrontCover_description"] + .Cre_banner_logo')) {
+          element.insertAdjacentHTML('afterend', $this.var.bannerHTML)
+        }
+      })
+       
+    },
+  },
+});
