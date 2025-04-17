a2d5aaaeabc68a713f10e5ff01893ecb61ade568
sarthakrautela <sarthakrautela80@gmail.com>
2025-04-17 13:35:42 +0530

    css

--- local_testing/Local2/variation/vB.js
+++ local_testing/Local2/variation/vB.js
@@ -1,14 +1,14 @@
-(function () {
+(function() {
     try {
         /* main variables */
         var debug = 1;
-        var variation_name = "cre-t-35";
-      /* helper library */
+        var variation_name = "cre-t-34";
+        /* helper library */
         var _$;
-        !(function (factory) {
+        !(function(factory) {
             _$ = factory();
-        })(function () {
-            var bm = function (s) {
+        })(function() {
+            var bm = function(s) {
                 if (typeof s === "string") {
                     this.value = Array.prototype.slice.call(document.querySelectorAll(s));
                 }
@@ -17,24 +17,24 @@
                 }
             };
             bm.prototype = {
-                eq: function (n) {
+                eq: function(n) {
                     this.value = [this.value[n]];
                     return this;
                 },
-                each: function (fn) {
+                each: function(fn) {
                     [].forEach.call(this.value, fn);
                     return this;
                 },
-                log: function () {
+                log: function() {
                     var items = [];
                     for (let index = 0; index < arguments.length; index++) {
                         items.push(arguments[index]);
                     }
                     console && console.log(variation_name, items);
                 },
-                addClass: function (v) {
+                addClass: function(v) {
                     var a = v.split(" ");
-                    return this.each(function (i) {
+                    return this.each(function(i) {
                         for (var x = 0; x < a.length; x++) {
                             if (i.classList) {
                                 i.classList.add(a[x]);
@@ -44,23 +44,23 @@
                         }
                     });
                 },
-                waitForElement: function (selector, trigger, delayInterval, delayTimeout) {
-                    var interval = setInterval(function () {
+                waitForElement: function(selector, trigger, delayInterval, delayTimeout) {
+                    var interval = setInterval(function() {
                         if (_$(selector).value.length) {
                             clearInterval(interval);
                             trigger();
                         }
                     }, delayInterval);
-                    setTimeout(function () {
+                    setTimeout(function() {
                         clearInterval(interval);
                     }, delayTimeout);
                 },
             };
-            return function (selector) {
+            return function(selector) {
                 return new bm(selector);
             };
         });
-  
+
         function live(selector, event, callback, context) {
             // helper for enabling IE 8 event bindings
             function addEvent(el, type, handler) {
@@ -70,13 +70,13 @@
             // matches polyfill
             this &&
                 this.Element &&
-                (function (ElementPrototype) {
+                (function(ElementPrototype) {
                     ElementPrototype.matches =
                         ElementPrototype.matches ||
                         ElementPrototype.matchesSelector ||
                         ElementPrototype.webkitMatchesSelector ||
                         ElementPrototype.msMatchesSelector ||
-                        function (selector) {
+                        function(selector) {
                             var node = this,
                                 nodes = (node.parentNode || node.document).querySelectorAll(selector),
                                 i = -1;
@@ -86,7 +86,7 @@
                 })(Element.prototype);
             // live binding helper using matchesSelector
             function live(selector, event, callback, context) {
-                addEvent(context || document, event, function (e) {
+                addEvent(context || document, event, function(e) {
                     var found,
                         el = e.target || e.srcElement;
                     while (el && el.matches && el !== context && !(found = el.matches(selector))) el = el.parentElement;
@@ -95,74 +95,81 @@
             }
             live(selector, event, callback, context);
         }
-  
+
+
+
+
         var helper = _$();
-  
-  
-  
-        function waitForTippy(trigger) {
-            var interval = setInterval(function () {
-                if (typeof window.tippy != "undefined") {
-                    clearInterval(interval);
-                    trigger();
-                }
-            }, 50);
-            setTimeout(function () {
-                clearInterval(interval);
-            }, 15000);
-        };
-  
-  
-        const priceGuaranteeHTML = `
-        <div class="price-guarantee">
-          <div class="guarantee-content">
-            <div class="icon">
-            <img class="cre-price-guarantee-badge" src="https://cdn-3.convertexperiments.com/uf/10007679/10007714/screenshot-2025-04-16-at-113809am_67ff51198c811.png" alt="price-guarantee-badge" />
 
-            </div>
-            <div class="text-content">
-              <h2>BEST PRICE GUARANTEE</h2>
-              <p>Prices are regulated by law. You won't find a lower price anywhere else for any policy found through this page.</p>
-            </div>
-            <button class="close-button" aria-label="Close">
-            <img class="X-cross-icon" src="https://cdn-3.convertexperiments.com/uf/10007679/10007714/vector-1_67ff659d1b9ae.png" alt="X-cross-icon" />
-
-            </button>
-          </div>
-        </div>
-      `;
-      
-     
-      
-  
-  
-        /* Variation Init */
         function init() {
-  
+
             _$("body").addClass(variation_name);
-            if (!document.querySelector(".price-guarantee")) {
-                 document.body.insertAdjacentHTML('beforeend', priceGuaranteeHTML);
-            }
-     
-     // Check if user has already closed the price guarantee box (session-based)
-        if (sessionStorage.getItem('priceGuaranteeClosed') !== 'true') {
-          document.querySelector('.price-guarantee').style.display = 'block';
-        } else {
-          document.querySelector('.price-guarantee').style.display = 'none';
+
+
+            const items = ['How We Rank', "FAQ's"];
+
+            const listHTML = `
+              ${items.map(item => `<li><a href="#content-section"> ${item}</a></li>`).join('')}
+            `;
+
+            // Target the first <li> inside the navigation
+            const firstNavItem = document.querySelector('.oxy-site-navigation.header-nav ul li:first-child');
+
+            helper.waitForElement('.oxy-site-navigation.header-nav ul li:first-child', function () {
+                if (firstNavItem) {
+                    const tempContainer = document.createElement('ul');
+                    tempContainer.innerHTML = listHTML;
+    
+                    // Insert each <li> before the first <li>
+                    Array.from(tempContainer.children).forEach(li => {
+                        firstNavItem.parentNode.insertBefore(li, firstNavItem);
+                    });
+                }
+            }, 50, 15000)
+           
+
+
+            const newListItem = document.createElement('li');
+            newListItem.classList.add('custom-nav-item_cre_get_quote');
+
+           
+            const link = document.createElement('a');
+            link.textContent = 'Get Quote';
+            link.href = '#_dynamic_list-27-63';
+
+            
+            newListItem.appendChild(link);
+
+            
+            helper.waitForElement('.oxy-site-navigation.header-nav ul li:last-child', function () {
+                const lastNavItem = document.querySelector('.oxy-site-navigation.header-nav ul li:last-child');
+                if (lastNavItem && lastNavItem.parentNode) {
+                    lastNavItem.parentNode.insertBefore(newListItem, lastNavItem.nextSibling);
+                }
+            }, 50, 15000)
+            
+
+            
+            const secondListItem = newListItem.cloneNode(true);
+
+            // Insert after .oxy-header-left a
+
+            helper.waitForElement('.oxy-header-left a', function () {
+                const headerLeftLink = document.querySelector('.oxy-header-left a');
+                if (headerLeftLink && headerLeftLink.parentNode) {
+                    headerLeftLink.parentNode.insertBefore(secondListItem, headerLeftLink.nextSibling);
+                }
+            }, 50, 15000)
+          
+
+
+
+
         }
-  
-        // Add event listener to close button
-        document.querySelector('.close-button').addEventListener('click', function () {
-          document.querySelector('.price-guarantee').style.display = 'none';
-          sessionStorage.setItem('priceGuaranteeClosed', 'true');
-        });
-  
-         
-        }
-  
+
         /* Init variation */
         helper.waitForElement('body', init, 25, 25000);
     } catch (e) {
         if (debug) console.log(e, "error in Test" + variation_name);
     }
-  })();
+})();
